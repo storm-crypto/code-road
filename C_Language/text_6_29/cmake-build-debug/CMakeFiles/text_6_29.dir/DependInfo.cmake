c28e607b9bd624dba64925cffe45b78d0a0b85eb
storm-crypto <1742593818@qq.com>
2022-07-01 15:48:53 +0800

    第一种分支语句if

--- C_Language/text_6_29/cmake-build-debug/CMakeFiles/text_6_29.dir/DependInfo.cmake
+++ C_Language/text_6_29/cmake-build-debug/CMakeFiles/text_6_29.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "D:/05code/2022/C_Language/text_6_29/main.c" "D:/05code/2022/C_Language/text_6_29/cmake-build-debug/CMakeFiles/text_6_29.dir/main.c.obj"
+  "D:/05code/2022/code-road/C_Language/text_6_29/main.c" "D:/05code/2022/code-road/C_Language/text_6_29/cmake-build-debug/CMakeFiles/text_6_29.dir/main.c.obj"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
