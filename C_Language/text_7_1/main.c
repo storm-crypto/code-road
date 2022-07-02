75cce184ec894572b00df685a4128591fa756d8c
storm-crypto <1742593818@qq.com>
2022-07-02 20:38:46 +0800

    练习：反向输出一个数字

--- C_Language/text_7_1/main.c
+++ C_Language/text_7_1/main.c
@@ -649,18 +649,18 @@
 //}
 
 // ¥Ú”°9*9≥À∑®±Ì
-int main()
-{
-    int i = 0;
-    int j = 0;
-
-    for (i = 1; i <= 9; i++)
-    {
-        for (j = 1; j <= i; j++)
-        {
-            printf("%d * %d = %2d     ", i, j, i*j);
-        }
-        printf("\n");
-    }
-    return 0;
-}
+//int main()
+//{
+//    int i = 0;
+//    int j = 0;
+//
+//    for (i = 1; i <= 9; i++)
+//    {
+//        for (j = 1; j <= i; j++)
+//        {
+//            printf("%d * %d = %2d  ", i, j, i*j);
+//        }
+//        printf("\n");
+//    }
+//    return 0;
+//}
