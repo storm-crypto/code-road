d5129db4137066d39a562090c265ce2bd083962e
storm-crypto <1742593818@qq.com>
2022-07-02 20:59:39 +0800

    练习：输入一个班级5个学生的5科成绩，输入5个学生的各科成绩和总分

--- C_Language/text_7_2/main.c
+++ C_Language/text_7_2/main.c
@@ -13,15 +13,37 @@
 //}
 
 // 练习：总成绩和平均分计算
+//int main()
+//{
+//    int i = 0;
+//    float score = 0.0;
+//    float sum = 0.0;
+//    for (i = 0; i < 3; i++)
+//    {
+//        scanf("%f", &score);
+//        sum += score;
+//    }
+//    printf("%.2f %.2f", sum, sum / 3);
+//}
+
+// 输入一个班级5个学生的5科成绩，输入5个学生的各科成绩和总分
 int main()
 {
+    float score[5] = {0.0};
     int i = 0;
-    float score = 0.0;
-    float sum = 0.0;
-    for (i = 0; i < 3; i++)
+    for (i = 0; i < 5; i++)
     {
-        scanf("%f", &score);
-        sum += score;
+        int j = 0;
+        float sum = 0.0;
+        for (j = 0; j < 5; j++) // 输入每个人的各科成绩
+        {
+            scanf("%f", &score[j]);
+            sum += score[j];
+        }
+        for (j = 0; j < 5; j++)
+        {
+            printf("%.1f ", score[j]);
+        }
+        printf("%.1f\n", sum);
     }
-    printf("%.2f %.2f", sum, sum / 3);
 }
