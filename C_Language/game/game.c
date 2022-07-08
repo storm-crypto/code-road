e9818766b118dc880d6ae6f4c1e57b965f2068a7
storm-crypto <1742593818@qq.com>
2022-07-08 23:10:55 +0800

    实现棋盘的展示函数Display

--- C_Language/game/game.c
+++ C_Language/game/game.c
@@ -3,7 +3,6 @@
 //
 
 #include "game.h"
-#include "game.h"
 // 初始化棋盘
 void InitBoard(char board[ROW][COL], int row, int col)
 {
@@ -19,16 +18,55 @@ void InitBoard(char board[ROW][COL], int row, int col)
 }
 
 // 打印棋盘
-void DisplayBoard(char board[ROW][COL], int row, int col)
-{
+//void DisplayBoard(char board[ROW][COL], int row, int col)
+//{
+//    int i = 0;
+//    for (i = 0; i < row; i++)
+//    {
+//        int j = 0;
+//        for (j = 0; j < col; j++)
+//        {
+//            printf("%c", board[i][j]);
+//        }
+//        printf("\n");
+//    }
+//}
+
+// 打印棋盘
+//void DisplayBoard(char board[ROW][COL], int row, int col)
+//{
+//    int i = 0;
+//    for (i = 0; i < row; i++)
+//    {
+//        // 打印数据
+//        printf(" %c | %c | %c \n", board[i][0], board[i][1], board[i][2]);
+//        // 打印分割行
+//        if (i < row - 1)
+//            printf("---|---|---\n");
+//    }
+//}
+void DisplayBoard(char board[ROW][COL], int row, int col) {
     int i = 0;
-    for (i = 0; i < row; i++)
-    {
+    for (i = 0; i < row; i++) {
+        // 打印数据
         int j = 0;
         for (j = 0; j < col; j++)
         {
-            printf("%c", board[i][j]);
+            printf(" %c ", board[i][j]);
+            if (j < col - 1)
+                printf("|");
         }
         printf("\n");
+        // 打印分割行
+        if (i < row - 1)
+        {
+            for (j = 0; j < col; j++)
+            {
+                printf("---");
+                if (j < col - 1)
+                    printf("|");
+            }
+            printf("\n");
+        }
     }
 }
