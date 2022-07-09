93d075ecdb260a6b5c3e5d4e953327d0b06b863d
storm-crypto <1742593818@qq.com>
2022-07-09 16:10:21 +0800

    打印棋盘，两个棋盘(优化版本，打印出对应的行和列)

--- C_Language/test_07_09/game.c
+++ C_Language/test_07_09/game.c
@@ -21,8 +21,14 @@ void show_board(char arr[ROWS][COLS], int row, int col)
 {
     int i = 0;
     int j = 0;
+    for (i = 0; i <= col; i++)
+    {
+        printf("%d ", i);
+    }
+    printf("\n");
     for (i = 1; i <= row; i++)
     {
+        printf("%d ", i);
         for (j = 1; j <= col; j++)
         {
             printf("%c ", arr[i][j]);
