179e5626f554369f9ea6c6cd7f2e7da90839eb1f
storm-crypto <1742593818@qq.com>
2022-07-11 08:56:50 +0800

    练习：求一个整数存储在内存中的二进制中1的个数

--- C_Language/test_07_11/main.c
+++ C_Language/test_07_11/main.c
@@ -13,17 +13,35 @@
 //}
 
 // 练习：不能创建临时变量，实现两个数的交换
+//int main()
+//{
+//    int a = 3;
+//    int b = 5;
+//    printf("a = %d b = %d\n", a, b);
+//
+//    a = a ^ b;
+//    b = a ^ b;
+//    a = a ^ b;
+//
+//    printf("a = %d b = %d\n",a, b);
+//
+//    return 0;
+//}
+
+// 练习：求一个整数存储在内存中的二进制中1的个数
 int main()
 {
-    int a = 3;
-    int b = 5;
-    printf("a = %d b = %d\n", a, b);
-
-    a = a ^ b;
-    b = a ^ b;
-    a = a ^ b;
-
-    printf("a = %d b = %d\n",a, b);
-
+    int num = 0;
+    scanf("%d", &num);
+    int i = 0;
+    int count = 0;
+    for (i = 0; i < 32; i++)
+    {
+        if ((num>>i) & 1)
+        {
+            count++;
+        }
+    }
+    printf("%d\n", count);
     return 0;
 }
