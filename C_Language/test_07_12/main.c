a006ea647534c5027818833c8c392e7f0a03dd9c
storm-crypto <1742593818@qq.com>
2022-07-12 15:05:04 +0800

    初识指针

--- C_Language/test_07_12/main.c
+++ C_Language/test_07_12/main.c
@@ -13,21 +13,72 @@
 //}
 
 // 用指针实现my_strlen
-int my_strlen(char* str)
-{
-    char* start = str;
-    while (*str)
-    {
-        str++;
-    }
-    return str - start;
-}
+//int my_strlen(char* str)
+//{
+//    char* start = str;
+//    while (*str)
+//    {
+//        str++;
+//    }
+//    return str - start;
+//}
+//
+//int main()
+//{
+//    char arr[] = "abcdef";
+//    int len = my_strlen(arr);
+//    printf("%d\n", len);
+//
+//    return 0;
+//}
+
+//int main()
+//{
+//    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//    int *p = arr;
+//    int sz = sizeof (arr) / sizeof (arr[0]);
+//    int i = 0;
+//    for (i = 0; i < sz; i++)
+//    {
+//        printf("%d ", *(p + i));
+//    }
+//
+//    return 0;
+//}
+
+//int main()
+//{
+//    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//    printf("%p\n", arr);
+//    printf("%p\n", arr + 1);
+//
+//    printf("%p\n", &arr[0]);
+//    printf("%p\n", &arr[0] + 1);
+//
+//    printf("%p\n", &arr);
+//    printf("%p\n", &arr + 1);
+//
+//    return 0;
+//}
+
+// 指针数组
 
 int main()
 {
-    char arr[] = "abcdef";
-    int len = my_strlen(arr);
-    printf("%d\n", len);
+    int data1[] = {1, 2, 3, 4, 5};
+    int data2[] = {2, 3, 4, 5, 6};
+    int data3[] = {3, 4, 5, 6, 7};
 
+    int* arr[3] = {data1, data2, data3};
+    int i = 0;
+    for (i = 0; i < 3; i++)
+    {
+        int j = 0;
+        for (j = 0; j < 5; j++)
+        {
+            printf("%d ", *(arr[i] + j));
+        }
+        printf("\n");
+    }
     return 0;
 }
