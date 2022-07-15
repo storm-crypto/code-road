c34d46ac93b60593ede8cb14172aa929f8776cd5
storm-crypto <1742593818@qq.com>
2022-07-15 09:57:29 +0800

    修改text_6_29为test_06_29

--- C_Language/test_07_14/main.c
+++ C_Language/test_07_14/main.c
@@ -2,23 +2,45 @@
 
 // 练习：设计一个小程序来判断当前机器的字节序
 
-int check_sys()
+//int check_sys()
+//{
+//    int a = 1;
+//    return *(char*)&a;
+//}
+//
+//int main()
+//{
+//    int ret = check_sys();
+//    if (ret == 1)
+//    {
+//        printf("小端\n");
+//    }
+//    else
+//    {
+//        printf("大端\n");
+//    }
+//
+//    return 0;
+//}
+
+// 函数栈帧的创建和销毁
+
+int Add(int a, int b)
 {
-    int a = 1;
-    return *(char*)&a;
+    int z = 0;
+    z = a + b;
+    return z;
 }
 
 int main()
 {
-    int ret = check_sys();
-    if (ret == 1)
-    {
-        printf("小端\n");
-    }
-    else
-    {
-        printf("大端\n");
-    }
+    int a = 10;
+    int b = 20;
+    int c = 0;
+
+    c = Add(a, b);
+
+    printf("%d\n", c);
 
     return 0;
 }
