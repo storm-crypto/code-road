6813fdb7845f6e6105090a11e65cfbe7dfc56bad
storm-crypto <1742593818@qq.com>
2022-07-18 16:06:15 +0800

    打印整数二进制的奇数位和偶数位

--- C_Language/test_07_18/main.c
+++ C_Language/test_07_18/main.c
@@ -3,29 +3,45 @@
 // 操作符部分的变成题目
 
 // 求两个数二进制中不同位的个数
-int cal_diff_bit(int m, int n)
+//int cal_diff_bit(int m, int n)
+//{
+//    int tmp = m ^ n;
+//    int count = 0;
+//    while (tmp)
+//    {
+//        tmp = tmp & (tmp - 1);
+//        count++;
+//    }
+//
+//    return count;
+//}
+//
+//int main()
+//{
+//    int m = 0;
+//    int n = 0;
+//
+//    while (scanf("%d%d", &m, &n) == 2)
+//    {
+//        int ret = cal_diff_bit(m, n);
+//        printf("%d\n", ret);
+//    }
+//
+//    return 0;
+//}
+
+// 打印整数二进制的奇数位和偶数位
+void Printbit(int num)
 {
-    int tmp = m ^ n;
-    int count = 0;
-    while (tmp)
+    int i = 0;
+
+    for (i = 31; i >= 1; i -= 2)
     {
-        tmp = tmp & (tmp - 1);
-        count++;
+        printf("%d ", (num >> i) & 1);
     }
 
-    return count;
-}
-
-int main()
-{
-    int m = 0;
-    int n = 0;
-
-    while (scanf("%d%d", &m, &n) == 2)
+    for (i = 30; i >= 0; i -= 2)
     {
-        int ret = cal_diff_bit(m, n);
-        printf("%d\n", ret);
+        printf("%d ", (num >> i) & 1);
     }
-
-    return 0;
 }
