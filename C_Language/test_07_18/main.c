760132d0e70af20feab75eaef36d04b47940a10a
storm-crypto <1742593818@qq.com>
2022-07-18 16:03:31 +0800

    求两个数二进制中不同位的个数

--- C_Language/test_07_18/main.c
+++ C_Language/test_07_18/main.c
@@ -1,47 +1,31 @@
 #include <stdio.h>
 
-//int main()
-//{
-//    int a, b, c;
-//    a = 5;
-//    c = ++a;
-//    b = ++c, c++, ++a, a++;
-//    b += a++ + c;
-//    printf("a = %d b = %d c = %d\n:", a, b, c);
-//    return 0;
-//}
+// ²Ù×÷·û²¿·ÖµÄ±ä³ÉÌâÄ¿
 
-// æ±‚ä¸¤ä¸ªæ•°äºŒè¿›åˆ¶ä¸­ä¸åŒä½çš„ä¸ªæ•°
-//int main()
-//{
-//    int a = 0;
-//    int b = 0;
-//    scanf("%d %d", &a, &b);
-//
-//    int i = 0;
-//    int count = 0;
-//    for (i = 0; i < 32; i++)
-//    {
-//        if (((a>>i) & 1) != ((b >> i) & 1))
-//        {
-//            count++;
-//        }
-//    }
-//    printf("%d", count);
-//
-//    return 0;
-//}
+// ÇóÁ½¸öÊı¶ş½øÖÆÖĞ²»Í¬Î»µÄ¸öÊı
+int cal_diff_bit(int m, int n)
+{
+    int tmp = m ^ n;
+    int count = 0;
+    while (tmp)
+    {
+        tmp = tmp & (tmp - 1);
+        count++;
+    }
 
-// ä½¿ç”¨æŒ‡é’ˆæ‰“å°æ•°ç»„çš„å†…å®¹
+    return count;
+}
 
 int main()
 {
-    int arr[] = {1, 2, 3, 4, 5};
-    int i = 0;
-    int* p = arr;
-    for (i = 0; i < 5; i++)
+    int m = 0;
+    int n = 0;
+
+    while (scanf("%d%d", &m, &n) == 2)
     {
-        printf("%d ", *(p + i));
+        int ret = cal_diff_bit(m, n);
+        printf("%d\n", ret);
     }
+
     return 0;
 }
