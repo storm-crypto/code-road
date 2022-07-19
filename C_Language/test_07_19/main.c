55a1fd79cfab6c10519971554769ae74543b2cbf
storm-crypto <1742593818@qq.com>
2022-07-19 16:04:21 +0800

    练习：使用指针打印数组内容

--- C_Language/test_07_19/main.c
+++ C_Language/test_07_19/main.c
@@ -58,50 +58,70 @@
 // 打印二维数组
 
 // 方法1：形参写成数组的形式
-void print1(int p[3][5], int r, int c)
-{
-    int i = 0;
-    for (i = 0; i < r; i ++)
-    {
-        int j = 0;
-        for (j = 0 ; j < c; j++)
-        {
-            printf("%d ", p[i][j]);
-            printf("\n");
-        }
-        printf("\n");
-    }
-}
-
+//void print1(int p[3][5], int r, int c)
+//{
+//    int i = 0;
+//    for (i = 0; i < r; i ++)
+//    {
+//        int j = 0;
+//        for (j = 0 ; j < c; j++)
+//        {
+//            printf("%d ", p[i][j]);
+//        }
+//        printf("\n");
+//    }
+//}
+//
 // 方法2：形参写成数组指针的形式
 // 达到了降维的效果
+//
+//void print2(int (*p)[5], int r, int c)
+//{
+//    int i = 0;
+//    for (i = 0; i < r; i ++)
+//    {
+//        int j = 0;
+//        for (j = 0 ; j < c; j++)
+//        {
+//            // p + i是指向第i行的
+//            // *(p + i)相当于拿到了第i行，也就是第i行的数组名
+//            // 数组名表示首元素的地址，*(p + i)就是表示拿到了第i行的地址
+//            printf("%d ", *(*(p + i) + j));
+//            // printf("%d ", p[i][j]);
+//        }
+//        printf("\n");
+//    }
+//
+//}
+//
+//int main()
+//{
+//    int arr[3][5] = {{1, 2, 3, 4, 5}, {2, 3, 4, 5, 6}, {3, 4, 5, 6, 7}};
+//
+//    print1(arr, 3, 5);
+//    print2(arr, 3, 5);
+//
+//    return 0;
+//}
 
-void print2(int (*p)[5], int r, int c)
+// 指针的概念，练习题目
+
+// 练习：使用指针打印数组内容
+void print1(int* arr, int sz)
 {
     int i = 0;
-    for (i = 0; i < r; i ++)
+    for (i = 0; i < sz; i++)
     {
-        int j = 0;
-        for (j = 0 ; j < c; j++)
-        {
-            // p + i是指向第i行的
-            // *(p + i)相当于拿到了第i行，也就是第i行的数组名
-            // 数组名表示首元素的地址，*(p + i)就是表示拿到了第i行的地址
-            printf("%d ", *(*(p + i) + j));
-            // printf("%d ", p[i][j]);
-            printf("\n");
-        }
-        printf("\n");
+        printf("%d ", *(arr + i));
     }
-
 }
 
 int main()
 {
-    int arr[3][5] = {{1, 2, 3, 4, 5}, {2, 3, 4, 5, 6}, {3, 4, 5, 6, 7}};
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int sz = sizeof (arr) / sizeof (arr[0]);
 
-    print1(arr, 3, 5);
-    print2(arr, 3, 5);
-
-    return 0;
+    print1(arr, sz);
 }
+
+
