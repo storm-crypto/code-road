d14cde28839b1bd1edcacbe3e95c99bd24678a8e
storm-crypto <1742593818@qq.com>
2022-07-19 09:45:28 +0800

    打印二维数组arr的2种方法

--- C_Language/test_07_19/main.c
+++ C_Language/test_07_19/main.c
@@ -8,49 +8,100 @@
 // 写一个函数打印arr数组的内容
 
 // 方法1：形参写成数组的形式
-void print1(int p[], int sz)
-{
-    int i = 0;
-    for (i = 0; i < sz; i++)
-    {
-        printf("%d ", p[i]);
-    }
-    printf("\n");
-}
-
+//void print1(int p[], int sz)
+//{
+//    int i = 0;
+//    for (i = 0; i < sz; i++)
+//    {
+//        printf("%d ", p[i]);
+//    }
+//    printf("\n");
+//}
+//
 // 方法2:形参写成指针的形式
-void print2(int* p, int sz)
+//void print2(int* p, int sz)
+//{
+//    int i = 0;
+//    for (i = 0; i < sz; i++)
+//    {
+//        printf("%d ", *(p + i));
+//    }
+//    printf("\n");
+//}
+//
+// 方法3：形参写成数组指针的形式
+//void print3(int (*p)[10], int sz)
+//{
+//    int i = 0;
+//    for (i = 0; i < sz; i++)
+//    {
+//        // *p相当于数组名，数组名又是首元素的地址，所以*p就是&arr[0]
+//        printf("%d ", *(*p + i));
+//    }
+//    printf("\n");
+//}
+//
+//int main()
+//{
+//
+//    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//
+//    int sz = sizeof (arr) / sizeof (arr[0]);
+//
+//    print1(arr, sz);
+//    print2(arr, sz);
+//    print3(&arr, sz);
+//
+//    return 0;
+//}
+
+// 打印二维数组
+
+// 方法1：形参写成数组的形式
+void print1(int p[3][5], int r, int c)
 {
     int i = 0;
-    for (i = 0; i < sz; i++)
+    for (i = 0; i < r; i ++)
     {
-        printf("%d ", *(p + i));
+        int j = 0;
+        for (j = 0 ; j < c; j++)
+        {
+            printf("%d ", p[i][j]);
+            printf("\n");
+        }
+        printf("\n");
     }
-    printf("\n");
 }
 
-// 方法3：形参写成数组指针的形式
-void print3(int (*p)[10], int sz)
+// 方法2：形参写成数组指针的形式
+// 达到了降维的效果
+
+void print2(int (*p)[5], int r, int c)
 {
     int i = 0;
-    for (i = 0; i < sz; i++)
+    for (i = 0; i < r; i ++)
     {
-        // *p相当于数组名，数组名又是首元素的地址，所以*p就是&arr[0]
-        printf("%d ", *(*p + i));
+        int j = 0;
+        for (j = 0 ; j < c; j++)
+        {
+            // p + i是指向第i行的
+            // *(p + i)相当于拿到了第i行，也就是第i行的数组名
+            // 数组名表示首元素的地址，*(p + i)就是表示拿到了第i行的地址
+            printf("%d ", *(*(p + i) + j));
+            // printf("%d ", p[i][j]);
+            printf("\n");
+        }
+        printf("\n");
     }
-    printf("\n");
+
 }
 
 int main()
 {
+    int arr[3][5] = {{1, 2, 3, 4, 5}, {2, 3, 4, 5, 6}, {3, 4, 5, 6, 7}};
 
-    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-    int sz = sizeof (arr) / sizeof (arr[0]);
-
-    print1(arr, sz);
-    print2(arr, sz);
-    print3(&arr, sz);
+    print1(arr, 3, 5);
+    print2(arr, 3, 5);
 
     return 0;
 }
