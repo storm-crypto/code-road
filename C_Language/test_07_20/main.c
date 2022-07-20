0346f161df74a726fbbefd01e1e46d999abe2565
storm-crypto <1742593818@qq.com>
2022-07-20 09:17:20 +0800

    练习：猜凶手

--- C_Language/test_07_20/main.c
+++ C_Language/test_07_20/main.c
@@ -1,37 +1,52 @@
 #include <stdio.h>
 
-// 鍚勭鐮佺殑缁冧範
+// 各种码的练习
 
-// 鎵撳嵃鏉ㄨ緣涓夎
+// 打印杨辉三角
 
-void yangHuiTriangle(int n)
-{
-    int data[30][30] = {1};
+//void yangHuiTriangle(int n)
+//{
+//    int data[30][30] = {1};
+//
+//    int i = 0;
+//    for (i = 1; i < n; i++)
+//    {
+//        data[i][0] = 1;
+//        int j = 0;
+//        for (j = 1; j <= i; j++)
+//        {
+//            data[i][j] = data[i - 1][j] + data[i - 1][j - 1];
+//        }
+//    }
+//
+//    // 打印
+//    for (i = 0; i < n; i++)
+//    {
+//        int j = 0;
+//        for (j = 0; j <= i; j++)
+//        {
+//            printf("%d ", data[i][j]);
+//        }
+//        printf("\n");
+//    }
+//}
+//int main()
+//{
+//    yangHuiTriangle(8);
+//    return 0;
+//}
 
-    int i = 0;
-    for (i = 1; i < n; i++)
-    {
-        data[i][0] = 1;
-        int j = 0;
-        for (j = 1; j <= i; j++)
-        {
-            data[i][j] = data[i - 1][j] + data[i - 1][j - 1];
-        }
-    }
-
-    // 鎵撳嵃
-    for (i = 0; i < n; i++)
-    {
-        int j = 0;
-        for (j = 0; j <= i; j++)
-        {
-            printf("%d ", data[i][j]);
-        }
-        printf("\n");
-    }
-}
+// 猜凶手
 int main()
 {
-    yangHuiTriangle(8);
+    int killer = 0;
+
+    for (killer = 'a'; killer <= 'd'; killer++)
+    {
+        if ((killer != 'a') + (killer == 'c') + (killer == 'd') + (killer != 'd') == 3)
+        {
+            printf("凶手是:%c", killer);
+        }
+    }
     return 0;
 }
