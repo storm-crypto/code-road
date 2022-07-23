b2bb8cceec358bbca91a8826e7b48d2356baea90
storm-crypto <1742593818@qq.com>
2022-07-23 19:05:38 +0800

    仿照qsort函数，改写一个通用版本的bubble_sort函数

--- C_Language/test_07_23/main.c
+++ C_Language/test_07_23/main.c
@@ -171,10 +171,56 @@
 
 // 使用qsort函数
 
-int cmp_int(const void* e1, const void* e2)
-{
-    return (*(int*) e1 - *(int*)e2);
-}
+//int cmp_int(const void* e1, const void* e2)
+//{
+//    return (*(int*) e1 - *(int*)e2);
+//}
+//
+//void print_arr(int arr[], int sz)
+//{
+//    int i = 0;
+//    for (i = 0; i < sz; i++)
+//    {
+//        printf("%d ", arr[i]);
+//    }
+//    printf("\n");
+//}
+//
+//void test2()
+//{
+//    int arr[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+//    // 排序为升序
+//    int sz = sizeof (arr) / sizeof (arr[0]);
+//    qsort(arr, sz, sizeof(arr[0]), cmp_int);
+//    print_arr(arr, sz);
+//}
+//
+//struct Stu
+//{
+//    char name[20];
+//    int age;
+//    double score;
+//};
+//
+//int cmp_stu_by_age(const void* e1, const void* e2)
+//{
+//    return ((struct Stu*)e1)->age - ((struct Stu*)e2)->age;
+//}
+//
+//int cmp_stu_by_name(const void* e1, const void* e2)
+//{
+//    return strcmp(((struct Stu*)e1)->name, ((struct Stu*)e2)->age);
+//}
+//
+//// 使用qsort排序结构体
+//void test3()
+//{
+//    struct Stu arr[3] = {{"zhangsan", 20, 55.5}, {"lisi", 20, 28.0}, {"wangwu", 50, 90.0}};
+//    int sz = sizeof (arr) / sizeof (arr[0]);
+//    qsort(arr, sz, sizeof (arr[0]), cmp_stu_by_age);
+//}
+
+// 仿照qsort函数，改写一个通用版本的bubble_sort函数
 
 void print_arr(int arr[], int sz)
 {
@@ -186,44 +232,56 @@ void print_arr(int arr[], int sz)
     printf("\n");
 }
 
+int cmp_int(const void* e1, const void* e2)
+{
+    return (*(int*) e1 - *(int*)e2);
+}
+
+void Swap(char* buf1, char* buf2, int width)
+{
+    int i = 0;
+    for (i = 0; i < width; i++)
+    {
+        char tmp = *buf1;
+        *buf1 = *buf2;
+        *buf2 = tmp;
+        buf1++;
+        buf2++;
+    }
+}
+
+void bubble_sort(void* base, int num, int width, int (*cmp)(const void* e1, const void* e2))
+{
+    int i = 0;
+    for (i = 0; i < num - 1; i++)
+    {
+        int j = 0;
+        for (j = 0; j < num - i - 1; j++)
+        {
+            // if (arr[j] > arr[j + 1])
+            if (cmp((char*)base + j * width, (char*)base + (j + 1) * width))
+            {
+//                int tmp = arr[j];
+//                arr[j] = arr[j + 1];
+//                arr[j + 1] = tmp;
+                  // 交换函数
+                  Swap((char*)base + j * width, (char*)base + (j + 1) * width, width);
+            }
+        }
+    }
+}
+
 void test2()
 {
     int arr[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
-    // 排序为升序
     int sz = sizeof (arr) / sizeof (arr[0]);
-    qsort(arr, sz, sizeof(arr[0]), cmp_int);
+    bubble_sort(arr, sz, sizeof (arr[0]), cmp_int);
     print_arr(arr, sz);
-}
-
-struct Stu
-{
-    char name[20];
-    int age;
-    double score;
-};
-
-int cmp_stu_by_age(const void* e1, const void* e2)
-{
-    return ((struct Stu*)e1)->age - ((struct Stu*)e2)->age;
-}
-
-int cmp_stu_by_name(const void* e1, const void* e2)
-{
-    return strcmp(((struct Stu*)e1)->name, ((struct Stu*)e2)->age);
-}
-
-// 使用qsort排序结构体
-void test3()
-{
-    struct Stu arr[3] = {{"zhangsan", 20, 55.5}, {"lisi", 20, 28.0}, {"wangwu", 50, 90.0}};
-    int sz = sizeof (arr) / sizeof (arr[0]);
-    qsort(arr, sz, sizeof (arr[0]), cmp_stu_by_age);
 }
 
 
 int main()
 {
-    //test2();
-    test3();
+    test2();
     return 0;
 }
