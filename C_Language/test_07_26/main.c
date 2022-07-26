23722c7463262fd6c4e2e95b9c396815315891b6
storm-crypto <1742593818@qq.com>
2022-07-26 17:08:23 +0800

    实现strcmp

--- C_Language/test_07_26/main.c
+++ C_Language/test_07_26/main.c
@@ -118,30 +118,60 @@
 //    return 0;
 //}
 
-char* my_strcat(char* dest, const char* src)
+//char* my_strcat(char* dest, const char* src)
+//{
+//    assert(dest && src);
+//    char* ret = dest;
+//    // 1. 找dest中的'\0'
+//    while (*dest)
+//    {
+//        dest++;
+//    }
+//
+//    // 2. 拷贝
+//    while (*dest ++ = *src++)
+//    {
+//        ;
+//    }
+//
+//    return ret;
+//}
+//
+//int main()
+//{
+//    char arr1[20] = "hello";
+//    char arr2[] = " bit";
+//    my_strcat(arr1, arr2);
+//
+//    printf("%s\n", arr1);
+//}
+
+// 实现strcmp
+
+int my_strcmp(const char* s1, const char* s2)
 {
-    assert(dest && src);
-    char* ret = dest;
-    // 1. 找dest中的'\0'
-    while (*dest)
+    assert(s1 && s2);
+    while (*s1 == * s2)
     {
-        dest++;
+        if (*s1 == '\0')
+        {
+            return 0;
+        }
+        s1++;
+        s2++;
     }
-
-    // 2. 拷贝
-    while (*dest ++ = *src++)
-    {
-        ;
-    }
-
-    return ret;
+    if (*s1 > *s2)
+        return 1;
+    else
+        return -1;
 }
 
 int main()
 {
-    char arr1[20] = "hello";
-    char arr2[] = " bit";
-    my_strcat(arr1, arr2);
+    char a1[] = "abc";
+    char a2[] = "abc";
+    int ret = my_strcmp(a1, a2);
+    printf("%d\n", ret);
 
-    printf("%s\n", arr1);
+    return 0;
 }
