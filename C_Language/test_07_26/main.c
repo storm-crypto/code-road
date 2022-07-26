984f0be9bf6548a7d40eb96301b49bdde9b85921
storm-crypto <1742593818@qq.com>
2022-07-26 08:30:08 +0800

    练习：变种水仙花，对pow的使用

--- C_Language/test_07_26/main.c
+++ C_Language/test_07_26/main.c
@@ -1,6 +1,24 @@
+#include <math.h>
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+
+// 练习：变种水仙花
+int main()
+{
+    int i = 0;
+    for (i = 10000; i <= 99999; i++)
+    {
+        int j = 0;
+        int sum = 0;
+        for (j = 0; j < 4; j++)
+        {
+            int m = i % (int)pow(10, j);
+            int n = i / (int)pow(10, j);
+            sum += m*n;
+        }
+        if (sum == i)
+        {
+            printf("%d ", i);
+        }
+    }
 }
