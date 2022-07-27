1cd622210f15a0ca2232a9d5b6947c3e47adc162
storm-crypto <1742593818@qq.com>
2022-07-27 08:31:20 +0800

    strtok函数的使用

--- C_Language/test_07_27/main.c
+++ C_Language/test_07_27/main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-    printf("Hello, World!\n");
+// strtok函数的使用
+int main()
+{
+    char arr[] = "zpengwei@yeah.net.hehe";
+    char buf[50] = {0};
+    strcpy(buf, arr);
+
+    const char* sep = "@.";
+    char* str = NULL;
+    for (str = strtok(buf, sep); str != NULL; str = strtok(NULL, sep))
+    {
+        printf("%s\n", str);
+    }
+
     return 0;
 }
