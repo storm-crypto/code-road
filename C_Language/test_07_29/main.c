c8c9100a3600864b556a1b0fb2a1ccdf84966647
storm-crypto <1742593818@qq.com>
2022-07-29 11:48:17 +0800

    fputs函数的简单使用（写文件-输出操作）

--- C_Language/test_07_29/main.c
+++ C_Language/test_07_29/main.c
@@ -26,21 +26,42 @@
 //}
 
 // 读文件
+//int main()
+//{
+//    // 打开文件
+//    FILE* pf = fopen("test.txt", "r");
+//    if (pf == NULL)
+//    {
+//        perror("fopen");
+//        return 1;
+//    }
+//    // 读文件 - 输入操作
+//    int ch = 0;
+//    while ((ch = getc(pf)) != EOF)
+//    {
+//        printf("%c ", ch);
+//    }
+//
+//    // 关闭文件
+//    fclose(pf);
+//    pf = NULL;
+//
+//    return 0;
+//}
+
 int main()
 {
     // 打开文件
-    FILE* pf = fopen("test.txt", "r");
+    FILE* pf = fopen("test.txt", "w");
     if (pf == NULL)
     {
         perror("fopen");
         return 1;
     }
-    // 读文件 - 输入操作
-    int ch = 0;
-    while ((ch = getc(pf)) != EOF)
-    {
-        printf("%c ", ch);
-    }
+
+    // 写文件  写一行
+    fputs("skajflsajdfl", pf);
+    fputs("xxxxxxxxxxxx", pf);
 
     // 关闭文件
     fclose(pf);
