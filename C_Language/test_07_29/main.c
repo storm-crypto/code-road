7e22e99f47b26c5662df4061f573d930786f7c66
storm-crypto <1742593818@qq.com>
2022-07-29 11:42:54 +0800

    读取文件test.txt中的信息，并打印到控制台上

--- C_Language/test_07_29/main.c
+++ C_Language/test_07_29/main.c
@@ -2,20 +2,44 @@
 
 
 // 打开和关闭文件
+//int main()
+//{
+//    // 打开文件
+//    FILE* pf = fopen("test.txt", "w");
+//    if (pf == NULL)
+//    {
+//        perror("fopen");
+//        return 1;
+//    }
+//    // 写文件 - 输出操作
+//    char i = 'a';
+//    for (i = 'a'; i <= 'z'; i++)
+//    {
+//        fputc(i, pf);
+//    }
+//
+//    // 关闭文件
+//    fclose(pf);
+//    pf = NULL;
+//
+//    return 0;
+//}
+
+// 读文件
 int main()
 {
     // 打开文件
-    FILE* pf = fopen("test.txt", "w");
+    FILE* pf = fopen("test.txt", "r");
     if (pf == NULL)
     {
         perror("fopen");
         return 1;
     }
-    // 写文件
-    char i = 'a';
-    for (i = 'a'; i <= 'z'; i++)
+    // 读文件 - 输入操作
+    int ch = 0;
+    while ((ch = getc(pf)) != EOF)
     {
-        fputc(i, pf);
+        printf("%c ", ch);
     }
 
     // 关闭文件
