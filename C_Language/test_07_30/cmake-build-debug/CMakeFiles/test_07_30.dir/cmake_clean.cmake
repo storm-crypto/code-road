83fe9498ba8fb4f498f1c0bd2db748fb6e915e30
storm-crypto <1742593818@qq.com>
2022-08-01 16:30:44 +0800

    练习：模拟实现atoi函数(完整考虑版本)

---
+++ C_Language/test_07_30/cmake-build-debug/CMakeFiles/test_07_30.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_07_30.dir/main.c.obj"
+  "libtest_07_30.dll.a"
+  "test_07_30.exe"
+  "test_07_30.exe.manifest"
+  "test_07_30.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test_07_30.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ C_Language/test_07_30/main.c
@@ -0,0 +1,92 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <stdio.h>
+
+// 模拟实现atoi
+//int my_atoi(const char* str)
+//{
+//    assert(str);
+//    int n = 0;
+//    while (*str)
+//    {
+//        n = n * 10 + *str - '0';
+//        str++;
+//    }
+//    return n;
+//}
+
+enum Status
+{
+    VALID,
+    INVALID
+}status = INVALID; // 非法的
+
+int my_atoi(const char* str)
+{
+    int flag = 0;
+    // 空指针
+    assert(str);
+
+    // 字符串为空
+    if (*str == '\0')
+    {
+        return 0;
+    }
+
+    // 前面有空白字符
+    while (isspace(*str))
+    {
+        str++;
+    }
+    // 正负号
+    if (*str == '+')
+    {
+        flag = 1;
+        str++;
+    }
+    else if (*str == '-')
+    {
+        flag = -1;
+        str++;
+    }
+    long long n = 0;
+    // 处理非数字字符
+    while (*str != '\0')
+    {
+        if (isdigit(*str))
+        {
+            n = n * 10 + flag * (*str - '0');
+            if (n < INT_MIN || n > INT_MAX)
+            {
+                n = 0;
+                break;
+            }
+        }
+        else
+        {
+            n = 0;
+            break;
+        }
+        str++;
+    }
+
+    // 字符串合法的情况
+    if (*str == '\0')
+    {
+        status = VALID;
+    }
+    return (int )n;
+}
+
+int main()
+{
+    char arr[20] = "1234";
+    int ret = my_atoi(arr);
+    if (status == VALID)
+        printf("合法转化%d\n", ret);
+    else
+        printf("非法转化:%d\n", ret);
+
+    return 0;
+}
