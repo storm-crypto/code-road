080efef490ad14afe9f25a7cf040654c354ff18c
storm-crypto <1742593818@qq.com>
2022-08-06 18:45:16 +0800

    定义链表的结构

---
+++ DataStructure/List/SList.c
@@ -0,0 +1,5 @@
+//
+// Created by Ðñ¸ç on 2022-08-06.
+//
+
+#include "SList.h"
---
+++ DataStructure/List/SList.h
@@ -0,0 +1,14 @@
+//
+// Created by Ðñ¸ç on 2022-08-06.
+//
+
+#ifndef LIST_SLIST_H
+#define LIST_SLIST_H
+
+struct SListNode
+{
+    int data;
+    struct SListNode* next;
+};
+
+#endif//LIST_SLIST_H
