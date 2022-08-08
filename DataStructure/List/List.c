fb7a43e14994ff7a79e524de2fb2dd6d08ea2d1a
storm-crypto <1742593818@qq.com>
2022-08-09 07:21:47 +0800

    有头双向循环链表 完成初始化链表接口

--- DataStructure/List/List.c
+++ DataStructure/List/List.c
@@ -3,3 +3,21 @@
 //
 
 #include "List.h"
+
+
+struct ListNode* BuyListNode(LTDataType x)
+{
+    ListNode* node = (ListNode*)malloc(sizeof (ListNode));
+    node->next = NULL;
+    node->prev = NULL;
+    return node;
+}
+
+ListNode* ListInit()
+{
+    ListNode* phead = BuyListNode(0);
+    phead->next = phead;
+    phead->prev = phead;
+
+    return phead;
+}
--- DataStructure/List/List.h
+++ DataStructure/List/List.h
@@ -5,13 +5,19 @@
 #ifndef LIST_LIST_H
 #define LIST_LIST_H
 
+#include <stdlib.h>
+#include <stdio.h>
+
 // 定义结构体
 typedef int LTDataType;
-struct ListNode
+typedef struct ListNode
 {
     struct ListNode* next;
     struct ListNode* prev;
     LTDataType data;
-};
+}ListNode;
+
+// 初始化
+ListNode* ListInit();
 
 #endif//LIST_LIST_H
