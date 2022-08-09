7d04471fe24a49f60a53b7c706a1fdb5ac92bbb6
storm-crypto <1742593818@qq.com>
2022-08-09 21:04:11 +0800

    数据结构：栈 测试栈的各个接口

--- DataStructure/StackQueue/main.c
+++ DataStructure/StackQueue/main.c
@@ -1,6 +1,28 @@
 #include "Stack.h"
 
-int main() {
-    printf("Hello, World!\n");
+void TestStack()
+{
+    Stack st;
+    StackInit(&st);
+    StackPush(&st, 1);
+    StackPush(&st, 2);
+    StackPush(&st, 3);
+    StackPush(&st, 4);
+
+    // ±È¿˙’ª
+    while (!StackEmpty(&st))
+    {
+        printf("%d ", StackTop(&st));
+        StackPop(&st);
+    }
+
+    StackDestory(&st);
+
+}
+
+int main()
+{
+    TestStack();
+
     return 0;
 }
