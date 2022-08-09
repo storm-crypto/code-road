41963eb8831ba821fca4f1f8d919f3d70ab810e2
storm-crypto <1742593818@qq.com>
2022-08-09 20:52:51 +0800

    数据结构：栈 2.实现栈的插入函数

--- DataStructure/StackQueue/Stack.c
+++ DataStructure/StackQueue/Stack.c
@@ -16,10 +16,36 @@ void StackInit(Stack* pst)
     pst->top = 0;
     pst->capacity = 4;
 }
-void StackDestory(Stack* pst);
+void StackDestory(Stack* pst)
+{
+    assert(pst);
+    free(pst->a);
+    pst->a = NULL;
+    pst->top = pst->capacity = 0;
+}
 
 // 性质规定决定了在栈顶出和入
-void StackPush(Stack* pst, STDataType x);
+void StackPush(Stack* pst, STDataType x)
+{
+    assert(pst);
+    // 增容
+    if (pst->top == pst->capacity)
+    {
+        STDataType* tmp = (STDataType*)realloc(pst->a, sizeof(STDataType)*(pst->capacity)*2);
+        if (tmp == NULL)
+        {
+            printf("realloc fail\n");
+            exit(-1); // 结束整个程序
+        }
+
+        pst->a = tmp;
+        pst->capacity *= 2;
+    }
+
+    pst->a[pst->top] = x;
+    pst->top++;
+
+}
 void StackPop(Stack* pst);
 
 // 取栈顶数据
