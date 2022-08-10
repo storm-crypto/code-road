2638af16a436e308af3606a92aafe64bd69dbbc5
storm-crypto <1742593818@qq.com>
2022-08-10 23:25:39 +0800

    数据结构：二叉树 实现创建一个节点的函数 创建了一棵二叉树

--- DataStructure/BinaryTree/main.c
+++ DataStructure/BinaryTree/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef int BTDataType;
+typedef char BTDataType;
 typedef struct BinaryTreeNode
 {
     struct BinaryTreeNode* left;
@@ -8,12 +9,38 @@ typedef struct BinaryTreeNode
     BTDataType data;
 }BTNode;
 
-void PrevOrder(BTNode* root);
+void PrevOrder(BTNode* root)
+{
+
+}
 void InOrder(BTNode* root);
 void PostOrder(BTNode* root);
 
+BTNode* CreateTreeNode(BTDataType x)
+{
+    BTNode* node = (BTNode*)malloc(sizeof(BTNode));
+    node->data = x;
+    node->left = NULL;
+    node->right = NULL;
+
+    return node;
+}
+
 int main()
 {
+    BTNode* A = CreateTreeNode('A');
+    BTNode* B = CreateTreeNode('B');
+    BTNode* C = CreateTreeNode('C');
+    BTNode* D = CreateTreeNode('D');
+    BTNode* E = CreateTreeNode('E');
+    BTNode* F = CreateTreeNode('F');
 
-    return 0;
+    A->left = B;
+    A->right = C;
+    B->left = D;
+    C->left = E;
+    C->right = F;
+
+    PrevOrder(A);
+
 }
