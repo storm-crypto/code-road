d2a104068061549e6638a3c86cb9c4d274e0ae6a
storm-crypto <1742593818@qq.com>
2022-08-10 16:47:35 +0800

    数据结构：堆 向下调整算法建小根堆

---
+++ DataStructure/Heap/cmake-build-debug/CMakeFiles/Heap.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Heap.dir/main.c.obj"
+  "Heap.exe"
+  "Heap.exe.manifest"
+  "Heap.pdb"
+  "libHeap.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Heap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ DataStructure/Heap/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void Swap(int* p1, int* p2)
+{
+    int tmp = *p1;
+    *p1 = *p2;
+    *p2 = tmp;
+}
+
+void AdjustDown(int* a, int n, int parent)
+{
+    int child = parent * 2 + 1;
+    while (child < n)
+    {
+        // 选出左右孩子中小的那个
+        if (child + 1 < n && a[child + 1] < a[child])
+        {
+            ++child;
+        }
+
+        if (a[child] < a[parent])
+        {
+            Swap(&a[parent], &a[child]);
+            parent = child;
+            child = parent * 2 + 1;
+        }
+        else
+        {
+            break;
+        }
+    }
+}
+
+int main()
+{
+    int a[] = {27,15,19,18,28,34,65,49,25,37};
+    int n = sizeof (a) / sizeof (a[0]);
+    AdjustDown(a, n, 0);
+    return 0;
+}
