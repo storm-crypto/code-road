e2fff7e3f3371cd6b4b12960781fa2bcc1ca269b
storm-crypto <1742593818@qq.com>
2022-08-10 09:43:13 +0800

    数据结构：队列 测试写好的接口

--- DataStructure/StackQueue/main.c
+++ DataStructure/StackQueue/main.c
@@ -1,4 +1,5 @@
 #include "Stack.h"
+#include "Queue.h"
 
 void TestStack()
 {
@@ -20,9 +21,29 @@ void TestStack()
 
 }
 
+void TestQueue()
+{
+    Queue q;
+    QueueInit(&q);
+    QueuePush(&q, 1);
+    QueuePush(&q, 2);
+    QueuePush(&q, 3);
+    QueuePush(&q, 4);
+
+    while (!QueueEmpty(&q))
+    {
+        printf("%d ", QueueFront(&q));
+        QueuePop(&q);
+    }
+
+    QueueDestroy(&q);
+
+}
+
 int main()
 {
-    TestStack();
+    // TestStack();
+    TestQueue();
 
     return 0;
 }
