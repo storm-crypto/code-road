87671fcd035e1819a06a2d4f0fffcd3a79d9663e
storm-crypto <1742593818@qq.com>
2022-08-10 08:53:56 +0800

    数据结构：队列 1.实现队列初始化 2.实现队列的销毁 3.实现队列的插入

--- DataStructure/StackQueue/Queue.c
+++ DataStructure/StackQueue/Queue.c
@@ -3,3 +3,50 @@
 //
 
 #include "Queue.h"
+
+void QueueInit(Queue* pq)
+{
+    assert(pq);
+    pq->head = pq->tail = NULL;
+}
+void QueueDestroy(Queue* pq)
+{
+    assert(pq);
+
+    QueueNode* cur = pq;
+    while (cur)
+    {
+        QueueNode* next = cur->next;
+        free(cur);
+        cur = cur->next;
+    }
+    pq->head = pq->tail = NULL;
+}
+
+void QueuePush(Queue* pq, QDataType x)
+{
+    assert(pq);
+    QueueNode* newnode = (QueueNode*)malloc(sizeof (QueueNode));
+    if (newnode == NULL)
+    {
+        printf("malloc fail\n");
+        exit(-1);
+    }
+    newnode->data = x;
+    newnode->next = NULL;
+    if (pq->tail == NULL)
+    {
+        pq->head = pq->tail = newnode;
+    }
+    else
+    {
+        pq->tail->next = newnode;
+        pq->tail = newnode;
+    }
+}
+void QueuePop(Queue* pq);
+// 取队头数据
+QDataType QueueFront(Queue* pq);
+QDataType QueueBack(Queue* pq);
+bool QueueEmpty(Queue* pq);
+int QueueSize(Queue* pq);
