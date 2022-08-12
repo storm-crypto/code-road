7c8d63d927d82db65ad9ab0f0919bc00a52a5171
storm-crypto <1742593818@qq.com>
2022-08-12 23:36:30 +0800

    排序算法：希尔排序 写了其中一个间隔的排序

--- DataStructure/Sort/Sort.c
+++ DataStructure/Sort/Sort.c
@@ -17,6 +17,7 @@ void Print(int* a, int n)
 // 插入排序
 void InsertSort(int* a, int n)
 {
+    // 多趟，因为end = n - 2的时候就排序结束了
     for (int i = 0; i < n - 1; i++)
     {
         // 把tmp插入到0~end的连续区间中
@@ -34,9 +35,29 @@ void InsertSort(int* a, int n)
                 break;
             }
         }
+        // 特殊情况，当end = -1的时候，下面这个式子同样适合
+        // 所以干脆直接else那里break掉，如果找到合适的位置就
+        // 将tmp填入这位置里面
         a[end + 1] = tmp;
     }
-
 }
 // 希尔排序
-void ShellSort(int* a, int n);
+void ShellSort(int* a, int n)
+{
+    int gap;
+    int end;
+    int tmp = a[end + gap];
+    while (end >= 0)
+    {
+        if (tmp < a[end])
+        {
+            a[end + gap] = a[end];
+            end -= gap;
+        }
+        else
+        {
+            break;
+        }
+    }
+    a[end + gap] = tmp;
+}
