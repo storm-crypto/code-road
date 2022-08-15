3de78466a238ae2fb338c0ab38b0cbb2b8336867
storm-crypto <1742593818@qq.com>
2022-08-15 10:32:29 +0800

    全缺省、半缺省

--- DataStructure/test_08_15/main.cpp
+++ DataStructure/test_08_15/main.cpp
@@ -56,13 +56,41 @@ using namespace std;
 //}
 
 // 缺省参数
-void TestFunc(int a = 0)
+//void TestFunc(int a = 0)
+//{
+//    cout << a << endl;
+//}
+//
+//int main()
+//{
+//    TestFunc();
+//    TestFunc(10);
+//}
+
+// 全缺省
+//void TestFunc(int a = 10, int b = 20, int c = 30)
+//{
+//    cout<<"a = "<<a<<endl;
+//    cout<<"b = "<<b<<endl;
+//    cout<<"c = "<<c<<endl << endl;
+//}
+//
+//int main()
+//{
+//    TestFunc(1, 2, 3);
+//    TestFunc();
+//    TestFunc(1);
+//}
+// 半缺省
+void TestFunc1(int a, int b = 20, int c = 30)
 {
-    cout << a << endl;
+    cout<<"a = "<<a<<endl;
+    cout<<"b = "<<b<<endl;
+    cout<<"c = "<<c<<endl << endl;
 }
 
 int main()
 {
-    TestFunc();
-    TestFunc(10);
+    TestFunc1(1, 2, 3);
+    TestFunc1(1);
 }
