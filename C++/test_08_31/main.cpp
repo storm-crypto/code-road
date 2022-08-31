68f381e8cc3b1ff3a04d7261df60a5b33f318c7a
storm-crypto <1742593818@qq.com>
2022-08-31 09:57:13 +0800

    在类里面重载<<，发现不符合要求

--- C++/test_08_31/main.cpp
+++ C++/test_08_31/main.cpp
@@ -39,6 +39,46 @@ using namespace std;
 //}
 
 
+//class Date
+//{
+//public:
+//    Date(int year = 0, int month = 1, int day = 1)
+//    {
+//        _year = year;
+//        _month = month;
+//        _day = day;
+//    }
+//
+//    // 了解一下
+//    // 他们基本没有被自己实现的价值
+//    // 可以不自己写，编译器默认生成的就够用了
+//    Date* operator&()
+//    {
+//        return this;
+//    }
+//
+//    const Date* operator&() const
+//    {
+//        return this;
+//    }
+//
+//
+//private:
+//    int _year;
+//    int _month;
+//    int _day;
+//};
+//
+//
+//int main()
+//{
+//    Date d1(2021, 5, 27);
+//    Date d2(2021, 5, 27);
+//
+//
+//    return 0;
+//}
+
 class Date
 {
 public:
@@ -49,19 +89,15 @@ public:
         _day = day;
     }
 
-    // 了解一下
-    // 他们基本没有被自己实现的价值
-    // 可以不自己写，编译器默认生成的就够用了
-    Date* operator&()
+    // 如果这样写的话，调用的时候要d1 << cout才能调用到这个函数
+    // 因为抵押给参数被this占用了
+    // 所以这种不符合我们的想法
+    // 所以写成全局的
+    void operator<<(ostream& out)
     {
-        return this;
+        out << _year << "-" << _month << "-" << _day << endl;
     }
-
-    const Date* operator&() const
-    {
-        return this;
-    }
-
+    operator>>();
 
 private:
     int _year;
@@ -72,9 +108,9 @@ private:
 
 int main()
 {
-    Date d1(2021, 5, 27);
-    Date d2(2021, 5, 27);
-
+    Date d1;
+    cin >> d1;
+    cout << d1;
 
     return 0;
 }
