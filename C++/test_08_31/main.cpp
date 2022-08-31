e156b51f4dbd82153859dd1bf0a075dca5983700
storm-crypto <1742593818@qq.com>
2022-08-31 09:42:52 +0800

    取地址和const取地址操作符重载

--- C++/test_08_31/main.cpp
+++ C++/test_08_31/main.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
 using namespace std;
 
+//class Date
+//{
+//public:
+//    Date(int year = 0, int month = 1, int day = 1)
+//    {
+//        _year = year;
+//        _month = month;
+//        _day = day;
+//    }
+//
+//    bool operator==(const Date& d) const
+//    {
+//        return _year == d._year
+//        && _month == d._month
+//        && _day == d._day;
+//    }
+//
+//    void Print() const
+//    {
+//        cout << _year << "年" << _month << "月" << _day << "日" << endl;
+//    }
+//private:
+//    int _year;
+//    int _month;
+//    int _day;
+//};
+//
+//
+//int main()
+//{
+//    Date d1(2021, 5, 27);
+//    Date d2(2021, 5, 27);
+//
+//
+//    return 0;
+//}
+
+
 class Date
 {
 public:
@@ -11,17 +49,20 @@ public:
         _day = day;
     }
 
-    bool operator==(const Date& d) const
+    // 了解一下
+    // 他们基本没有被自己实现的价值
+    // 可以不自己写，编译器默认生成的就够用了
+    Date* operator&()
     {
-        return _year == d._year
-        && _month == d._month
-        && _day == d._day;
+        return this;
     }
 
-    void Print() const
+    const Date* operator&() const
     {
-        cout << _year << "年" << _month << "月" << _day << "日" << endl;
+        return this;
     }
+
+
 private:
     int _year;
     int _month;
@@ -32,7 +73,7 @@ private:
 int main()
 {
     Date d1(2021, 5, 27);
-    Date d1(2021, 5, 27);
+    Date d2(2021, 5, 27);
 
 
     return 0;
