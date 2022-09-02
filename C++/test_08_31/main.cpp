980a48dd5bc35c591292d8893e4e559f00d633b0
storm-crypto <1742593818@qq.com>
2022-09-02 09:35:55 +0800

    函数模板参数的匹配规则，跟非函数模板一起对比学习

--- C++/test_08_31/main.cpp
+++ C++/test_08_31/main.cpp
@@ -444,26 +444,60 @@ using namespace std;
 
 // 泛型编程 -- 模板
 
-// 函数模板
-// template<class T>
-template<typename T> // 定义模板参数T可以用typename，也可以用class，都一样，一般用class
-void Swap(T&x1, T&x2)
+//// 函数模板
+//// template<class T>
+//template<typename T> // 定义模板参数T可以用typename，也可以用class，都一样，一般用class
+//void Swap(T&x1, T&x2)
+//{
+//    T tmp = x1;
+//    x1 = x2;
+//    x2 = tmp;
+//}
+//
+//int main()
+//{
+//    int a = 1, b = 2;
+//    Swap(a, b);
+//
+//    double c = 1.1, d = 2.2;
+//    Swap(c, d);
+//
+//
+//    return 0;
+//}
+
+//// 专门处理int的加法函数
+//int Add(int left, int right)
+//{
+//    return left + right;
+//}
+//// 通用加法函数
+//template<class T>
+//T Add(T left, T right)
+//{
+//    return left + right;
+//}
+//void Test()
+//{
+//    Add(1, 2); // 与非模板函数匹配，编译器不需要特化
+//    Add<int>(1, 2); // 调用编译器特化的Add版本
+//}
+
+// 专门处理int的加法函数
+int Add(int left, int right)
 {
-    T tmp = x1;
-    x1 = x2;
-    x2 = tmp;
+    return left + right;
 }
-
-int main()
+// 通用加法函数
+template<class T1, class T2>
+T1 Add(T1 left, T2 right)
 {
-    int a = 1, b = 2;
-    Swap(a, b);
-
-    double c = 1.1, d = 2.2;
-    Swap(c, d);
-
-
-    return 0;
+    return left + right;
+}
+void Test()
+{
+    Add(1, 2); // 与非函数模板类型完全匹配，不需要函数模板实例化
+    Add(1, 2.0); // 模板函数可以生成更加匹配的版本，编译器根据实参生成更加匹配的Add函数
 }
 
 
