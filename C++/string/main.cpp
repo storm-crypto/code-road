bb1ced4404d5eb4affa83eaa46f94ab2bf1b0bd9
storm-crypto <1742593818@qq.com>
2022-09-02 15:53:31 +0800

    string的遍历方式： 1. 下标 + []

--- C++/string/main.cpp
+++ C++/string/main.cpp
@@ -8,21 +8,16 @@ int main()
     // 三种常见的构造函数
     string s1;
     string s2("hello world");
-    string s3(s2);
-    string s4 = "hello world"; // 这种方式跟s2的那种构造方法是一样的
 
-    // 拷贝已存在字符串的部分子串
-    string s5(s4, 0, 5);
-    cout << s5 << endl;
+    // 三种遍历方式
+    // 1. 下标 + [] 也是最常用的一种遍历方式
+    for (size_t i = 0; i < s2.size(); i++)
+    {
+        // 会转化成s2.operator[](i)
+        cout << s2[i] << " ";
+    }
+    cout << endl;
 
-    string s6("123456", 3);
-    cout << s6 << endl;
-
-    string s7(10, 'x');
-    cout << s7 << endl;
-
-    s7 = s2;
-    cout << s7 << endl;
 
     return 0;
 }
