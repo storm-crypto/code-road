977439474420c8b6234aa1f9afad65aaaa9b7001
storm-crypto <1742593818@qq.com>
2022-09-05 19:39:23 +0800

    vector的三种遍历方式： 1. 范围for 2. 迭代器 3. 靠size的遍历

---
+++ C++/vector/cmake-build-debug/CMakeFiles/vector.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vector.dir/main.cpp.obj"
+  "libvector.dll.a"
+  "vector.exe"
+  "vector.exe.manifest"
+  "vector.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ C++/vector/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void test_vector1()
+{
+    vector<int> v1;
+    v1.push_back(1);
+    v1.push_back(2);
+    v1.push_back(3);
+    v1.push_back(4);
+
+    // 遍历1
+    for (size_t i = 0; i < v1.size(); ++i)
+    {
+        cout << v1[i] << " ";
+    }
+    cout << endl;
+
+    // 遍历2
+    vector<int>::iterator it1 = v1.begin();
+    while (it1 != v1.end())
+    {
+        cout << *it1 << " ";
+        ++it1;
+    }
+    cout << endl;
+
+    // 遍历3(自动判断结束，自动迭代++)
+    // 看起来很牛逼，实际上就是被替换成了迭代器
+    for (auto e : v1)
+    {
+        cout << e << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    test_vector1();
+
+    return 0;
+}
