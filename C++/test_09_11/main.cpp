dbc523116a6d6610c83051e08364118c0b6b0e33
storm-crypto <1742593818@qq.com>
2022-09-11 16:12:16 +0800

    二进制写操作，用ofstream

--- C++/test_09_11/main.cpp
+++ C++/test_09_11/main.cpp
@@ -3,59 +3,63 @@
 
 using namespace std;
 
-void ReadFile()
-{
-	ifstream ifs("main.cpp");
-	char ch;
-	while (ifs)
-	{
-		cout << (char)ifs.get();
-	}
-}
-
-void WriteFile()
-{
-	ofstream ofs("write.txt");
-	ofs.put('x');
-	ofs.write("hello world", 10);
-}
-
-int main()
-{
-	ReadFile();
-	WriteFile();
-
-	return 0;
-}
-
-//struct ServerInfo
+//void ReadFile()
 //{
-//	char _ip[20];
-//	int _port;
-//};
-//
-//struct ConfigManger
-//{
-//public:
-//	ConfigManger(const char* filename)
-//		:_filename(filename)
-//	{}
-//	void ReadBin(ServerInfo& info);
-//	void WriteBin(const ServerInfo& info)
+//	ifstream ifs("main.cpp");
+//	char ch;
+//	while (ifs)
 //	{
-//		ofstream ofs(_filename);
-//		ofs.write((char*)&info, sizeof(info));
+//		cout << (char)ifs.get();
 //	}
-//private:
-//	string _filename;
-//};
+//}
+//
+//void WriteFile()
+//{
+//	ofstream ofs("write.txt");
+//	ofs.put('x');
+//	ofs.write("hello world", 10);
+//}
 //
 //int main()
 //{
-//	ServerInfo winfo = {"192.0.0.1", 80};
-//	ConfigManger cfbin("config.bin");
-//	// 读写 二进制
-//	cfbin.WriteBin(winfo);
+//	ReadFile();
+//	WriteFile();
 //
-//	// 读写 文本
+//	return 0;
 //}
+
+struct ServerInfo
+{
+	char _ip[20];
+	int _port;
+};
+
+struct ConfigManger
+{
+public:
+	ConfigManger(const char* filename)
+		:_filename(filename)
+	{}
+	void ReadBin(ServerInfo& info)
+	{
+		ifstream ifs(_filename);
+		ifs.read((char*)&info, sizeof(info));
+	}
+	void WriteBin(const ServerInfo& info)
+	{
+		ofstream ofs(_filename);
+		ofs.write((char*)&info, sizeof(info));
+	}
+private:
+	string _filename;
+};
+
+int main()
+{
+	ServerInfo winfo = {"192.0.0.1", 80};
+	ConfigManger cfbin("config.bin");
+	// 读写 二进制
+	cfbin.WriteBin(winfo);
+
+	// 读写 文本
+}
