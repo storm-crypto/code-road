0abcba8b412bdf17dfb7c10db45780f96f58cc74
storm-crypto <1742593818@qq.com>
2022-09-12 11:05:44 +0800

    虚函数重写的例外：协变

--- C++/test_9_12/main.cpp
+++ C++/test_9_12/main.cpp
@@ -15,49 +15,88 @@ using namespace std;
 //	return 0;
 //}
 
+//class Person
+//{
+//public:
+//	virtual void BuyTicket()
+//	{
+//		cout << "买票-全价" << endl;
+//	}
+//};
+//
+//class Student : public Person
+//{
+//public:
+//	// 子类重写父类的虚函数：
+//	virtual void BuyTicket()
+//	{
+//		cout << "买票-半价" << endl;
+//	}
+//};
+//
+//class Soldier : public Person
+//{
+//	// 子类重写父类的虚函数：
+//public:
+//	virtual void BuyTicket()
+//	{
+//		cout << "优先买票" << endl;
+//	}
+//};
+//
+//void f(Person& p)
+//{
+//	p.BuyTicket();
+//}
+//
+//int main()
+//{
+//	Person p; // 普通人
+//	Student st; // 学生
+//	Soldier so; // 军人
+//
+//	f(p);
+//	f(st);
+//	f(so);
+//
+//	return 0;
+//}
+
+class A{};
+class B : public A {};
+
 class Person
 {
 public:
-	virtual void BuyTicket()
+	virtual A* f()
 	{
-		cout << "买票-全价" << endl;
+		cout << " A* Person::f()" << endl;
+		return new A;
 	}
 };
 
 class Student : public Person
 {
 public:
-	// 子类重写父类的虚函数：
-	virtual void BuyTicket()
+	virtual B* f()
 	{
-		cout << "买票-半价" << endl;
+		cout << " B* Student::f()" << endl;
+		return new B;
 	}
 };
 
-class Soldier : public Person
-{
-	// 子类重写父类的虚函数：
-public:
-	virtual void BuyTicket()
-	{
-		cout << "优先买票" << endl;
-	}
-};
-
-void f(Person& p)
-{
-	p.BuyTicket();
-}
 
 int main()
 {
-	Person p; // 普通人
-	Student st; // 学生
-	Soldier so; // 军人
+	Person p;
+	Student s;
 
-	f(p);
-	f(st);
-	f(so);
+	Person* ptr;
+	ptr = &p;
+	ptr->f();
+
+	ptr = &s;
+	ptr->f();
 
 	return 0;
 }
