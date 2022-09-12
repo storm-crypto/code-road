6df88f8e368c95df4d3df0939e0c9d0602ceed60
storm-crypto <1742593818@qq.com>
2022-09-13 07:52:28 +0800

    定义了二叉搜索树的类，包括节点类，和头节点那些

--- C++/BSTree/BinarySearchTree.h
+++ C++/BSTree/BinarySearchTree.h
@@ -5,4 +5,25 @@
 #ifndef BSTREE__BINARYSEARCHTREE_H
 #define BSTREE__BINARYSEARCHTREE_H
 
+#include <iostream>
+using namespace std;
+
+template<class K>
+struct BSTreeNode
+{
+	BSTreeNode<K> *_left;
+	BSTreeNode<K> *_right;
+
+	K _key;
+};
+
+template<class K>
+class BSTree
+{
+	typedef BSTreeNode<K> Node;
+
+private:
+	Node * _root;
+};
+
 #endif //BSTREE__BINARYSEARCHTREE_H
