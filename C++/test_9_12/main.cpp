4a4b477a81d6847782a0eca2aab9456379c35c3a
storm-crypto <1742593818@qq.com>
2022-09-12 12:30:22 +0800

    虚函数重写的例外：析构函数的重写

--- C++/test_9_12/main.cpp
+++ C++/test_9_12/main.cpp
@@ -62,41 +62,73 @@ using namespace std;
 //	return 0;
 //}
 
-class A{};
-class B : public A {};
+//class A{};
+//class B : public A {};
+//
+//class Person
+//{
+//public:
+//	virtual A* f()
+//	{
+//		cout << " A* Person::f()" << endl;
+//		return new A;
+//	}
+//};
+//
+//class Student : public Person
+//{
+//public:
+//	virtual B* f()
+//	{
+//		cout << " B* Student::f()" << endl;
+//		return new B;
+//	}
+//};
+//
+//
+//int main()
+//{
+//	Person p;
+//	Student s;
+//
+//	Person* ptr;
+//	ptr = &p;
+//	ptr->f();
+//
+//	ptr = &s;
+//	ptr->f();
+//
+//	return 0;
+//}
 
 class Person
 {
 public:
-	virtual A* f()
+	// 建议把父类的析构函数定义为虚函数
+	// 这样子类的虚函数方便重写父类的虚函数
+	virtual ~Person()
 	{
-		cout << " A* Person::f()" << endl;
-		return new A;
+		cout << "~Person()" << endl;
 	}
 };
 
 class Student : public Person
 {
 public:
-	virtual B* f()
+	// Student和person析构函数的函数名，看起来不相同，但是他们构成虚函数重写
+	virtual ~Student()
 	{
-		cout << " B* Student::f()" << endl;
-		return new B;
+		cout << "~Student" << endl;
 	}
 };
 
-
 int main()
 {
+	// 在普通场景下，父子类的析构函数是否构成重写，不重要，不影响
 	Person p;
 	Student s;
 
-	Person* ptr;
-	ptr = &p;
-	ptr->f();
 
-	ptr = &s;
-	ptr->f();
 
 	return 0;
 }
