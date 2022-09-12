48c302a472f4b98059bb3bbb32fdc827fa29381b
storm-crypto <1742593818@qq.com>
2022-09-12 10:49:52 +0800

    重写虚函数

--- C++/test_9_12/main.cpp
+++ C++/test_9_12/main.cpp
@@ -24,6 +24,26 @@ public:
 	}
 };
 
+class Student : public Person
+{
+public:
+	// 子类重写父类的虚函数：
+	virtual void BuyTicket()
+	{
+		cout << "买票-半价" << endl;
+	}
+};
+
+class Soldier : public Person
+{
+	// 子类重写父类的虚函数：
+public:
+	virtual void BuyTicket()
+	{
+		cout << "优先买票" << endl;
+	}
+};
+
 int main()
 {
 
