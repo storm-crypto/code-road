5ec782aa5936bfc8ff0305abd5220ae07b8360de
storm-crypto <1742593818@qq.com>
2022-09-12 10:29:39 +0800

    静态的多态：函数重载

---
+++ C++/test_9_12/cmake-build-debug/CMakeFiles/test_9_12.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_9_12.dir/main.cpp.obj"
+  "libtest_9_12.dll.a"
+  "test_9_12.exe"
+  "test_9_12.exe.manifest"
+  "test_9_12.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_9_12.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ C++/test_9_12/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int i;
+	char ch;
+	cin >> i;
+	cin >> ch;
+
+	cout << ch << endl;
+	cout << i << endl;
+
+	return 0;
+}
