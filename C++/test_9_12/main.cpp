62da65ff5f5f35335739c3c6bde060e92ac1ffec
storm-crypto <1742593818@qq.com>
2022-09-12 10:32:53 +0800

    虚函数的指出

--- C++/test_9_12/main.cpp
+++ C++/test_9_12/main.cpp
@@ -2,15 +2,31 @@
 
 using namespace std;
 
+//int main()
+//{
+//	int i;
+//	char ch;
+//	cin >> i;
+//	cin >> ch;
+//
+//	cout << ch << endl;
+//	cout << i << endl;
+//
+//	return 0;
+//}
+
+class Person
+{
+public:
+	virtual void BuyTicket()
+	{
+		cout << "ÂòÆ±-È«¼Û" << endl;
+	}
+};
+
 int main()
 {
-	int i;
-	char ch;
-	cin >> i;
-	cin >> ch;
 
-	cout << ch << endl;
-	cout << i << endl;
 
 	return 0;
 }
