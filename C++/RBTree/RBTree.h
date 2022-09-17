1d36a8068d6b9f4283e98514bf078eb93ff7b95f
storm-crypto <1742593818@qq.com>
2022-09-18 01:18:55 +0800

    红黑树的实现： 实现插入：针对笔记中做的情况1、2、3都实现了，就剩下跟他们相反方向的那种情况没有实现

--- C++/RBTree/RBTree.h
+++ C++/RBTree/RBTree.h
@@ -85,8 +85,128 @@ public:
 		cur = newnode;
 
 		// 红黑树的控制逻辑
+		// 需要处理时候的情况：父亲存在，且颜色为红色需要持续处理
+		while (parent && parent->_col == Red)
+		{
+			// 关键看叔叔
+			Node* grandfather = parent->_parent;
+			if (parent == grandfather->_left)
+			{
+				Node* uncle = grandfather->_right;
+				// 情况1：uncle存在且为红
+				if (uncle && uncle->_col == Red)
+				{
+					// 把parent和uncle变黑，祖父变红
+					parent->_col = uncle->_col = Black;
+					grandfather->_col = Red;
+
+					// 继续往上处理
+					cur = grandfather;
+					parent = cur->_parent;
+				}
+				else // 情况2 + 3:uncle不存在，或者uncle存在且为黑
+				{
+					// cur是父亲的左边：情况2：单旋
+					if (cur == parent->_left)
+					{
+						RotateR(grandfather);
+						grandfather->_col = Red;
+						parent->_col = Black;
+					}
+					else // cur是父亲的右边：情况3：双旋
+					{
+						RotateL(parent);
+						RotateL(grandfather);
+						cur->_col = Black;
+						grandfather->_col = Red;
+					}
+					break;
+				}
+			}
+			else // parent == grandfather->_right 跟上面的是反方向的
+			{
+				// ...
+			}
+		}
+
+		// 最后这里做一个处理：把根的颜色变成黑
+		_root->_col = Black;
 
 		return make_pair(newnode, true);
+	}
+
+	// 左单旋
+	void RotateL(Node* parent)
+	{
+		Node* subR = parent->_right;
+		Node* subRL = subR->_left;
+
+		parent->_right = subRL;
+		if (subRL)
+		{
+			subRL->_parent = parent;
+		}
+		subR->_left = parent;
+		Node* parentParent = parent->_parent;
+		parent->_parent = subR;
+
+		if (parent == _root)
+		{
+			_root = subR;
+			subR->_parent = nullptr;
+		}
+		else
+		{
+			if (parentParent->_left == parent)
+			{
+				parentParent->_left = subR;
+			}
+			else
+			{
+				parentParent->_right = subR;
+			}
+			subR->_parent = parentParent;
+		}
+
+	}
+	// 右单旋
+	void RotateR(Node* parent)
+	{
+		Node* subL = parent->_left;
+		Node* subLR = subL->_right;
+
+		parent->_left = subLR;
+		// 注意为空的情况
+		if (subLR)
+		{
+			subLR->_parent = parent;
+		}
+
+		subL->_right = parent;
+		// 记录一下parent的parent的位置
+		Node* parentParent = parent->_parent;
+		parent->_parent = subL;
+
+		// 如果是独立的树进行旋转：
+		if (parent == _root)
+		{
+			_root = subL;
+			_root->_parent = nullptr;
+		}
+		else // 旋转的部分是子树的情况
+		{
+			if (parentParent->_left == parent)
+			{
+				parentParent->_left = subL;
+			}
+			else
+			{
+				parentParent->_right = subL;
+			}
+
+			subL->_parent = parentParent;
+		}
+
 	}
 private:
 	Node* _root;
