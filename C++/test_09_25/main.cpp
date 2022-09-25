223db16636df148c8c91fa0355742ffd711b6244
storm-crypto <1742593818@qq.com>
2022-09-25 15:18:43 +0800

    特殊类设计： 单例模式： 1. 其他版本的懒汉模式

--- C++/test_09_25/main.cpp
+++ C++/test_09_25/main.cpp
@@ -93,36 +93,87 @@ private:
 
 // 懒汉模式
 
+//class Singleton
+//{
+//public:
+//	// 懒汉模式可能会出现
+//	static Singleton* GetInstance()
+//	{
+//		// 保护第一次需要加锁，后面都不需要加锁的场景，可以使用双检查加锁
+//		// 特点：第一次加锁，后面不加锁，保护了线程安全，同时提高了效率
+//		if (_inst == nullptr)
+//		{
+//			_mtx.lock();
+//			if (_inst == nullptr)
+//			{
+//				_inst = new Singleton;
+//			}
+//			_mtx.unlock();
+//		}
+//
+//		return _inst;
+//	}
+//
+//	static void DelInstance()
+//	{
+//		_mtx.lock();
+//		if (_inst)
+//		{
+//			delete _inst;
+//			_inst = nullptr;
+//		}
+//		_mtx.unlock();
+//	}
+//
+//private:
+//	// 构造函数私有化
+//	Singleton()
+//	{}
+//
+//	~Singleton()
+//	{
+//		// 程序结束时，需要处理一下，持久化保存一下数据
+//	}
+//
+//	Singleton(const Singleton&) = delete;
+//	Singleton& operator=(const Singleton&) = delete;
+//
+//	// 实现一个内嵌垃圾回收类
+//	class CGarbo
+//	{
+//	public:
+//		~CGarbo()
+//		{
+//			if (_inst)
+//			{
+//				delete _inst;
+//				_inst = nullptr;
+//			}
+//		}
+//	};
+//
+//	static Singleton* _inst;
+//
+//	static std::mutex _mtx;
+//
+//	static CGarbo _gc;
+//};
+//
+//Singleton* Singleton::_inst = nullptr;
+//std::mutex Singleton::_mtx;
+//Singleton::CGarbo Singleton::_gc;
+
+// 其他版本的懒汉
+// 单例对象在静态区，如果单例对象太大，不太好
+// 如果想主动释放单例对象，无法主动控制
 class Singleton
 {
 public:
 	// 懒汉模式可能会出现
 	static Singleton* GetInstance()
 	{
-		// 保护第一次需要加锁，后面都不需要加锁的场景，可以使用双检查加锁
-		// 特点：第一次加锁，后面不加锁，保护了线程安全，同时提高了效率
-		if (_inst == nullptr)
-		{
-			_mtx.lock();
-			if (_inst == nullptr)
-			{
-				_inst = new Singleton;
-			}
-			_mtx.unlock();
-		}
-
-		return _inst;
-	}
-
-	static void DelInstance()
-	{
-		_mtx.lock();
-		if (_inst)
-		{
-			delete _inst;
-			_inst = nullptr;
-		}
-		_mtx.unlock();
+		static Singleton inst;
+		return &inst;
 	}
 
 private:
@@ -138,30 +189,8 @@ private:
 	Singleton(const Singleton&) = delete;
 	Singleton& operator=(const Singleton&) = delete;
 
-	// 实现一个内嵌垃圾回收类
-	class CGarbo
-	{
-	public:
-		~CGarbo()
-		{
-			if (_inst)
-			{
-				delete _inst;
-				_inst = nullptr;
-			}
-		}
-	};
-
-	static Singleton* _inst;
-
-	static std::mutex _mtx;
-
-	static CGarbo _gc;
 };
 
-Singleton* Singleton::_inst = nullptr;
-std::mutex Singleton::_mtx;
-Singleton::CGarbo Singleton::_gc;
 
 int main()
 {
