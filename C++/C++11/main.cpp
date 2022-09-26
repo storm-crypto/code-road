33485cdcbc7ebe625ae1f0de9ceb0ec1afb71a2c
storm-crypto <1742593818@qq.com>
2022-09-26 20:03:19 +0800

    C++11: {}初始化：std::initializer_list的使用场景：主要用于容器的初始化

--- C++/C++11/main.cpp
+++ C++/C++11/main.cpp
@@ -7,12 +7,12 @@
 
 using namespace std;
 
-struct Point
-{
-	int _x;
-	int _y;
-};
-
+//struct Point
+//{
+//	int _x;
+//	int _y;
+//};
+//
 class Date
 {
 public:
@@ -28,26 +28,42 @@ private:
 	int _month;
 	int _day;
 };
+//
+//int main()
+//{
+//	// C++11开始进行赋值的时候也可以不使用=
+//	Point p1 = {1, 2};
+//	Point p2{1, 2};
+//
+//	// 甚至初始化整型的时候也可以不用等号
+//	int x1 = 1;
+//	int x2{2};
+//
+//	int* p3 = new int[4]{0};
+//	int* p4 = new int[4]{1, 2, 3, 4};
+//
+//	Date d1;
+//	Date d2(2022, 9, 27);
+//	// 这里虽然可以这样用，但是不建议这样使用，这种做法是为其他地方做准备的
+//	Date d3{2022, 9, 27};
+//	Date d4 = {2022, 9, 27};
+//
+//
+//	return 0;
+//}
 
 int main()
 {
-	// C++11开始进行赋值的时候也可以不使用=
-	Point p1 = {1, 2};
-	Point p2{1, 2};
+	// the type of il is an initializer_list
+	auto il = {10, 20, 30};
+	std::initializer_list<int> il2 = {1, 2, 3, 4};
+	cout << typeid(il).name() << endl;
 
-	// 甚至初始化整型的时候也可以不用等号
-	int x1 = 1;
-	int x2{2};
+	vector<int> v = {1, 2, 3, 4, 5};
+	list<int> lt {10, 20, 30};
 
-	int* p3 = new int[4]{0};
-	int* p4 = new int[4]{1, 2, 3, 4};
-
-	Date d1;
-	Date d2(2022, 9, 27);
-	// 这里虽然可以这样用，但是不建议这样使用，这种做法是为其他地方做准备的
-	Date d3{2022, 9, 27};
-	Date d4 = {2022, 9, 27};
-
+	vector<Date> vd = {Date(2022, 9, 27), Date{2022, 9, 27}, {2022, 9, 27}};
+	map<string, int> dict = {make_pair("sort", 2), {"insert", 2}};
 
 	return 0;
 }
