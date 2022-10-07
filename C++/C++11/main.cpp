ae3228a714f8838c2a849078001ef7ce740d4cb6
storm-crypto <1742593818@qq.com>
2022-10-07 19:41:24 +0800

    C++11: 包装器 包装器的使用方法

--- C++/C++11/main.cpp
+++ C++/C++11/main.cpp
@@ -515,36 +515,93 @@ void Fun(const int &&x){ cout << "const 右值引用" << endl; }
 //}
 
 // 包装器
-//ret = func(x);
-// 上面func可能是什么呢？那么func可能是函数名？函数指针？函数对象(仿函数对象)？也有可能
-// 是lamber表达式对象？所以这些都是可调用的类型！如此丰富的类型，可能会导致模板的效率低下！
-// 为什么呢？我们继续往下看
-template<class F, class T>
-T useF(F f, T x)
+////ret = func(x);
+//// 上面func可能是什么呢？那么func可能是函数名？函数指针？函数对象(仿函数对象)？也有可能
+//// 是lamber表达式对象？所以这些都是可调用的类型！如此丰富的类型，可能会导致模板的效率低下！
+//// 为什么呢？我们继续往下看
+//template<class F, class T>
+//T useF(F f, T x)
+//{
+//	static int count = 0;
+//	cout << "count:" << ++count << endl;
+//	cout << "count:" << &count << endl;
+//	return f(x);
+//}
+//double f(double i)
+//{
+//	return i / 2;
+//}
+//struct Functor
+//{
+//	double operator()(double d)
+//	{
+//		return d / 3;
+//	}
+//};
+//int main()
+//{
+//	// 函数名
+//	cout << useF(f, 11.11) << endl;
+//	// 函数对象
+//	cout << useF(Functor(), 11.11) << endl;
+//	// lamber表达式
+//	cout << useF([](double d)->double{ return d/4; }, 11.11) << endl;
+//	return 0;
+//}
+
+// 包装器的使用方法：
+#include <functional>
+
+int f(int a, int b)
 {
-	static int count = 0;
-	cout << "count:" << ++count << endl;
-	cout << "count:" << &count << endl;
-	return f(x);
+	return a + b;
 }
-double f(double i)
-{
-	return i / 2;
-}
+
 struct Functor
 {
-	double operator()(double d)
+public:
+	int operator() (int a, int b)
 	{
-		return d / 3;
+		return a + b;
+	}
+};
+
+class Plus
+{
+public:
+	static int plusi(int a, int b)
+	{
+		return a + b;
+	}
+	double plusd(double a, double b)
+	{
+		return a + b;
 	}
 };
 int main()
 {
-	// 函数名
-	cout << useF(f, 11.11) << endl;
+	// 函数名(函数指针)
+	std::function<int(int, int)> func1 = f;
+	cout << func1(1, 2) << endl;
+
 	// 函数对象
-	cout << useF(Functor(), 11.11) << endl;
-	// lamber表达式
-	cout << useF([](double d)->double{ return d/4; }, 11.11) << endl;
+	std::function<int(int, int)> func2 = Functor();
+	cout << func2(1, 2) << endl;
+
+	// lambda表达式
+	std::function<int(int, int)> func3 = [](const int a, const int b)
+	{return a + b; };
+	cout << func3(1, 2) << endl;
+
+	// 类的成员函数
+
+	// 静态成员，直接调用就行了
+	std::function<int(int, int)> func4 = &Plus::plusi;
+	cout << func4(1, 2) << endl;
+
+	// 非静态成员，用function的时候要多加一个参数Plus
+	std::function<double(Plus, double, double)> func5 = &Plus::plusd;
+	cout << func5(Plus(), 1.1, 2.2) << endl;
+
 	return 0;
 }
