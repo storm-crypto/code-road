062ff053a8846ccb673a9571c7f1ee9a5442ed5e
storm-crypto <1742593818@qq.com>
2022-10-07 20:50:46 +0800

    C++11特性： 线程库： 之前的那个commit，参数传的是函数指针，这个传的是lambda表达式的形式，交替打印

--- C++/test_10_07/main.cpp
+++ C++/test_10_07/main.cpp
@@ -4,17 +4,39 @@
 #include <mutex>
 using namespace std;
 
-mutex mtx;
+//mutex mtx;
+//
+//void f(int N)
+//{
+//	mtx.lock();
+//	for (int i = 0; i < N; i++)
+//	{
+//		cout << this_thread::get_id() << ":" << i << endl;
+//	}
+//	mtx.unlock();
+//}
+//
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//
+//	vector<thread> vthreads;
+//	vthreads.resize(n);
+//	for (auto& td : vthreads)
+//	{
+//		td = thread(f, 100);
+//	}
+//
+//	for (auto& td : vthreads)
+//	{
+//		td.join();
+//	}
+//
+//	return 0;
+//}
 
-void f(int N)
-{
-	mtx.lock();
-	for (int i = 0; i < N; i++)
-	{
-		cout << this_thread::get_id() << ":" << i << endl;
-	}
-	mtx.unlock();
-}
+// 用lambda表达式来玩，上面那种是函数指针的方式来玩的
 
 int main()
 {
@@ -23,15 +45,29 @@ int main()
 
 	vector<thread> vthreads;
 	vthreads.resize(n);
+	mutex mtx;
+	int N = 100;
+	int x = 0;
 	for (auto& td : vthreads)
 	{
-		td = thread(f, 100);
+		// 串行
+		td = thread([&mtx, &N, &x]
+					{
+						mtx.lock();
+						for (int i = 0; i < N; i++)
+						{
+							++x;
+						}
+						mtx.unlock();
+					});
 	}
 
 	for (auto& td : vthreads)
 	{
 		td.join();
 	}
+
+	printf("%d个线程并行对x++了%d次, x:%d", n, N, x);
 
 	return 0;
 }
