81c5e7191b3cfd6cc06dc3e407bbf202b4e3b601
storm-crypto <1742593818@qq.com>
2022-10-10 00:26:04 +0800

    简单认识一下fork

--- linux/lesson8/myproc.c
+++ linux/lesson8/myproc.c
@@ -4,10 +4,16 @@
 
 int main()
 {
-  while(1){
-    printf("hello bit!: pid : %d, ppid : %d\n", getpid(), getppid());
-    sleep(1);
-  }  
+  fork();
+
+  printf("hello world\n");
+
+  sleep(2);
+
+  //while(1){
+  //  printf("hello bit!: pid : %d, ppid : %d\n", getpid(), getppid());
+  //  sleep(1);
+  //}  
 
   return 0;
 }
