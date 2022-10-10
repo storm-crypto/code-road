26905526138b94099038c81d6d04e10e015c71bd
storm-crypto <1742593818@qq.com>
2022-10-10 20:21:03 +0800

    1.编写代码来验证各种状态(R S T)

--- linux/lesson9/myproc.cc
+++ linux/lesson9/myproc.cc
@@ -1,34 +1,57 @@
 #include<iostream>
 #include<unistd.h>
 
+using namespace std;
+
 int main()
 {
   pid_t id = fork();
   if (id == 0)
   {
     // child
-    while (true)
+    while (1)
     {
-      std::cout << "I am child, pid: " << getpid() << ", ppid:" << getppid() << std::endl;
-      sleep(1);
-    }
-  }
-  else if (id > 0)
-  {
-    // parent
-    while (true)
-    {
-
-      std::cout << "I am parent, pid: " << getpid() << ", ppid:" << getppid() << std::endl;
+      cout << "I am child, running!" << endl;
       sleep(2);
     }
   }
-  else
+  else 
   {
-    // TODU
+    // parent
+    cout << "father do nothing" << endl;
+    sleep(50);
+
   }
+//  while (true)
+//  {
+//    std::cout << "Hello world" << std::endl;
+//  }
+  //pid_t id = fork();
+  //if (id == 0)
+  //{
+  //  // child
+  //  while (true)
+  //  {
+  //    std::cout << "I am child, pid: " << getpid() << ", ppid:" << getppid() << std::endl;
+  //    sleep(1);
+  //  }
+  //}
+  //else if (id > 0)
+  //{
+  //  // parent
+  //  while (true)
+  //  {
 
-  sleep(1);
+  //    std::cout << "I am parent, pid: " << getpid() << ", ppid:" << getppid() << std::endl;
+  //    sleep(2);
+  //  }
+  //}
+  //else
+  //{
+  //  // TODU
+  //}
 
-  return 0;
+  //sleep(1);
+
+  //return 0;
 }
