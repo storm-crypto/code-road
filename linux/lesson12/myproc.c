d6b38e7419e316e741f145e89839a9f5826151ec
storm-crypto <1742593818@qq.com>
2022-10-13 22:29:52 +0800

    exit的作用

--- linux/lesson12/myproc.c
+++ linux/lesson12/myproc.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
+int fun()
+{
+  printf("exit\n");
+  return 11;
+}
 
 int main()
 {
+  fun();
+  exit(66);
   for (int i = 0; i < 100; i++)
   {
     printf("%d:%s\n", i, strerror(i));
