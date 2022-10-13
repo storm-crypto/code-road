4f687b51c025630616b7181218bc5eb980c5075f
storm-crypto <1742593818@qq.com>
2022-10-14 00:42:05 +0800

    利用宏，不用自己操作status参数来获取退出码和信号

--- linux/lesson12/myproc.c
+++ linux/lesson12/myproc.c
@@ -26,19 +26,32 @@ int main()
  // pid_t ret = wait(NULL);
  
   int status = 0;
+ 
   pid_t ret = waitpid(id, &status, 0); 
-  if (ret > 0){
-    printf("father wait: %d, success\n, status exit code: %d, status exit signal: %d\n", ret, (status>>8)&0xFF, status&0x7F);
+  if (ret > 0)
+  {
+    if (WIFEXITED(status))
+    {
+      // 正常退出，获取对应的退出码
+      printf("exit code: %d\n", WEXITSTATUS(status));
+    }
+    else
+    {
+      printf("error, get a signal!\n");
+    }
   }
-  else{
-    printf("father wait failed!\n");
-  }
-  printf("father wait: %d\n", ret);
+//  if (ret > 0){
+//    printf("father wait: %d, success\n, status exit code: %d, status exit signal: %d\n", ret, (status>>8)&0xFF, status&0x7F);
+//  }
+//  else{
+//    printf("father wait failed!\n");
+//  }
+//  printf("father wait: %d\n", ret);
 
 
-  // 另一个状态，父进程把子进程回收完后，自己还活上10s
-  sleep(10);
-}
+ // // 另一个状态，父进程把子进程回收完后，自己还活上10s
+ // sleep(10);
+}//
 
 
 
