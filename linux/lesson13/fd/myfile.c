78b5c06684be12833752681408d9deaf043b9369
storm-crypto <1742593818@qq.com>
2022-10-18 15:19:18 +0800

    复习C语言读文件的操作

--- linux/lesson13/fd/myfile.c
+++ linux/lesson13/fd/myfile.c
@@ -2,17 +2,32 @@
 
 int main()
 {
-  FILE *fp = fopen("./log.txt", "w");
+  // FILE *fp = fopen("./log.txt", "w");
+  FILE *fp = fopen("./log.txt", "r");
   if (NULL == fp)
   {
     perror("fopen");
     return 1;
   }
-  
-  int cnt = 10;
-  while (cnt--){
-    const char *msg = "hello bit\n";
-    fputs(msg, fp);
+
+  char buffer[64];
+  while (fgets(buffer, sizeof(buffer), fp))
+  {
+    printf("%s", buffer);
   }
+  
+  // 判断文件是否是正常退出
+  if (!feof(fp)){
+    printf("fgets quit not normal!\n");
+  }
+  else{
+    printf("fgets quit normal!\n");
+  }
+  
+  //int cnt = 10;
+  //while (cnt--){
+  //  const char *msg = "hello bit\n";
+  //  fputs(msg, fp);
+  //}
   fclose(fp);
 }
