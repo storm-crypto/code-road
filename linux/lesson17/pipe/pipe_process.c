2209a676c25c60f34f6cbb9a9830a6ba3fad37dc
storm-crypto <1742593818@qq.com>
2022-11-11 16:40:03 +0800

    利用管道， 父进程从管道中读取数据，子进程将数据写入到管道中，父进程一直读取数据，子进程写入之后一次之后，就会sleep一下

---
+++ linux/lesson17/pipe/pipe_process.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int main()
+{
+    int pipefd[2] = {0};
+    if (pipe(pipefd) != 0) {
+        perror("pipe error");
+        return 1;
+    }
+
+    printf("pipefd[0]:%d\n", pipefd[0]); // fd=3
+    printf("pipefd[1]:%d\n", pipefd[1]); // fd=4
+
+    // 我们想让父进程进行读取，子进程写入
+    // pipefd: 0(嘴):读取 1(笔)：写入   
+    if (fork() == 0){
+        // 子进程 -- 写入
+        close(pipefd[0]);
+
+        const char* msg = "hello 比特";
+        while (1)
+        {
+            write(pipefd[1], msg, strlen(msg));
+            sleep(1);
+        }
+
+        exit(0);
+    }
+    // 父进程 -- 读取
+    close(pipefd[1]);
+    while (1)
+    {
+        char buffer[64];
+        // 如果read的返回值是0，意味着子进程关闭文件描述符了
+        // read的返回值是表示读取了多少个字节
+        ssize_t s = read(pipefd[0], buffer, sizeof (buffer));
+        if (s == 0){
+            break;
+        }
+        else if (s > 0)
+        {
+            buffer[s] = 0;
+            printf("child say:%s\n", buffer);
+        }
+        else 
+        {
+            break;
+        }
+    }
+
+    return 0;
+}
