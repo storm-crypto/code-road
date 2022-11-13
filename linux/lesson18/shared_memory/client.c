843e69cf758dcb7d4962bd10ebf37df38c520222
storm-crypto <1742593818@qq.com>
2022-11-13 15:11:40 +0800

    共享内存写逻辑代码来测试他有什么特点 逻辑代码：client不断地往shm中写入数据，server读取shm中的数据

--- linux/lesson18/shared_memory/client.c
+++ linux/lesson18/shared_memory/client.c
@@ -21,14 +21,24 @@ int main()
     }
 
     char *mem = (char *)shmat(shmid, NULL, 0);
-    sleep(5);
+    // sleep(5);
     printf("client process attaches success!\n");
 
     // 这个地方就是我们要通信的区域
+    // 不断地往共享内存mem中写入字符串
+    char c = 'A';
+    while (c <= 'Z')
+    {
+        mem[c - 'A'] = c;
+        c++;
+        mem[c - 'A'] = 0; // 0表示字符串的结尾
+        sleep(2);
+    }
+
 
     shmdt(mem);
     printf("client process detaches succss\n");
-    sleep(5);
+    // sleep(5);
     
 
     return 0;
--- linux/lesson18/shared_memory/server.c
+++ linux/lesson18/shared_memory/server.c
@@ -19,20 +19,28 @@ int main()
 
     printf("key: %u, shmid: %d\n", key, shmid);
 
-    sleep(1);
+    // sleep(1);
 
     char *mem = (char *)shmat(shmid, NULL, 0);
     printf("attaches shm success\n");
-    sleep(10);
+    // sleep(15);
+
+    // 这里就是我后面要进行的通信逻辑
+    while (1)
+    {
+        sleep(1);
+        // 这里有没有调用类似pipe or fifo中的read这样的结果呢？
+        printf("%s\n", mem); // server认为共享内存里面放的是一个长字符串
+    }
 
     shmdt(mem);
     printf("detaches shm success\n");
-    sleep(5);
+    // sleep(5);
 
     shmctl(shmid, IPC_RMID, NULL); // 销毁共享内存
     printf("key: 0x%x, shmid: %d -> shm delete success\n", key, shmid);
 
-    sleep(10);
+    // sleep(10);
 
     return 0;
 }
