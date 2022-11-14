a4b56a72ca4b9910c481d47da55f27b7cafb8585
storm-crypto <1742593818@qq.com>
2022-11-15 02:36:41 +0800

    使用kill函数，产生信号

--- linux/lesson19/test.c
+++ linux/lesson19/test.c
@@ -2,42 +2,69 @@
 #include <unistd.h>
 #include <signal.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-void handler(int signo)
+// kill命令的使用手册，当参数不够的时候会显示这个手册
+static void Usage(const char *proc)
 {
-    switch(signo)
-    {
-        case 2:
-            printf("hello bit, get a signal: %d\n", signo);
-            break;
-        case 3:
-            printf("hello world, get a signal: %d\n", signo);
-            break;
-        case 9:
-            printf("hello ..., get a signal: %d\n", signo);
-            break;
-        default:
-            printf("get signo: %d\n", signo);
-            break;
-    }
-    exit(1);
+    printf("Usage:\n\t %s signo who \n", proc);
 }
 
-int main()
+// .mytest signo who
+int main(int argc, char *argv[])
 {
-    // 将所有的信号都进程捕捉
-    int sig = 1;
-    for (; sig < 32; sig++)
+    if (argc != 3)
     {
-        signal(sig, handler);
+        Usage(argv[0]);
+        return 1;
     }
 
-    while (1)
-    {
-        int p = 10;
-        p /= 0;
-        printf("hello bit\n");
-        sleep(1);
-    }
+    int signo = atoi(argv[1]);
+    int who = atoi(argv[2]);
+
+    kill(who, signo);
+
+    printf(":signo: %d, who: %d\n", signo, who);
+
     return 0;
 }
+
+// void handler(int signo)
+// {
+//     switch(signo)
+//     {
+//         case 2:
+//             printf("hello bit, get a signal: %d\n", signo);
+//             break;
+//         case 3:
+//             printf("hello world, get a signal: %d\n", signo);
+//             break;
+//         case 9:
+//             printf("hello ..., get a signal: %d\n", signo);
+//             break;
+//         default:
+//             printf("get signo: %d\n", signo);
+//             break;
+//     }
+//     exit(1);
+// }
+
+// int main()
+// {
+//     // 将所有的信号都进程捕捉
+//     // int sig = 1;
+//     // for (; sig < 32; sig++)
+//     // {
+//     //     signal(sig, handler);
+//     // }
+
+//     while (1)
+//     {
+//         int p = 10;
+//         p /= 0; 
+//         printf("hello bit\n");
+//         sleep(1);
+//     }
+//     return 0;
+// }
