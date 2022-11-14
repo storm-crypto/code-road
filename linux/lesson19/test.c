0ec2549730d7272cd1678850287dbebd0e61dc51
storm-crypto <1742593818@qq.com>
2022-11-15 01:10:35 +0800

    会出现段错误的代码

--- linux/lesson19/test.c
+++ linux/lesson19/test.c
@@ -17,12 +17,15 @@ void handler(int signo)
             printf("hello ..., get a signal: %d\n", signo);
             break;
         default:
+            printf("get signo: %d\n", signo);
             break;
     }
+    exit(1);
 }
 
 int main()
 {
+    // 将所有的信号都进程捕捉
     int sig = 1;
     for (; sig < 32; sig++)
     {
@@ -31,6 +34,9 @@ int main()
 
     while (1)
     {
+        int *p = NULL;
+        p = (int*)100; // 会出现段错误，导致进程奔溃
+        *p = 100;
         printf("hello bit\n");
         sleep(1);
     }
