391366d8b70e421696acefab4928f25074f06699
storm-crypto <1742593818@qq.com>
2022-11-15 02:51:43 +0800

    abort()函数，发送信号

--- linux/lesson19/test.c
+++ linux/lesson19/test.c
@@ -11,15 +11,42 @@ static void Usage(const char *proc)
     printf("Usage:\n\t %s signo who \n", proc);
 }
 
+void handler(int signo)
+{
+    switch(signo)
+    {
+        case 2:
+            printf("hello bit, get a signal: %d\n", signo);
+            break;
+        case 3:
+            printf("hello world, get a signal: %d\n", signo);
+            break;
+        case 9:
+            printf("hello ..., get a signal: %d\n", signo);
+            break;
+        default:
+            printf("get signo: %d\n", signo);
+            break;
+    }
+    exit(1);
+}
+
 // .mytest signo who
 int main(int argc, char *argv[])
 {
+    // 捕获所有信号
+    int sig = 1;
+    for (; sig < 32; sig++)
+    {
+        signal(sig, handler);
+    }
+    
     while (1)
     {
         printf("I am a process\n");
         sleep(3);
 
-        raise(8);
+        abort();
     }
     return 0;
 }
