6929aabf09b6f32dc9d1d20dc72d104db213b3da
storm-crypto <1742593818@qq.com>
2022-11-15 15:39:37 +0800

    demo：统计1s钟可以count++多少次，每次count++都跟进行io操作

--- linux/lesson19/test.c
+++ linux/lesson19/test.c
@@ -5,54 +5,66 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-// kill命令的使用手册，当参数不够的时候会显示这个手册
-static void Usage(const char *proc)
-{
-    printf("Usage:\n\t %s signo who \n", proc);
-}
+int count = 0;
 
-void handler(int signo)
+int main()
 {
-    switch(signo)
-    {
-        case 2:
-            printf("hello bit, get a signal: %d\n", signo);
-            break;
-        case 3:
-            printf("hello world, get a signal: %d\n", signo);
-            break;
-        case 9:
-            printf("hello ..., get a signal: %d\n", signo);
-            break;
-        default:
-            printf("get signo: %d\n", signo);
-            break;
-    }
-    exit(1);
-}
+    alarm(1);
 
-// .mytest signo who
-int main(int argc, char *argv[])
-{
-    // 捕获所有信号
-    int sig = 1;
-    for (; sig < 32; sig++)
-    {
-        signal(sig, handler);
-    }
-
-    int res = alarm(30);
-    
     while (1)
     {
-        printf("I am a process\n");
-        sleep(1);
-
-        int res = alarm(0); // 取消闹钟
-        printf("res:%d\n", res);
+        printf("hello : %d\n", count++);
     }
-    return 0;
 }
+
+// // kill命令的使用手册，当参数不够的时候会显示这个手册
+// static void Usage(const char *proc)
+// {
+//     printf("Usage:\n\t %s signo who \n", proc);
+// }
+
+// void handler(int signo)
+// {
+//     switch(signo)
+//     {
+//         case 2:
+//             printf("hello bit, get a signal: %d\n", signo);
+//             break;
+//         case 3:
+//             printf("hello world, get a signal: %d\n", signo);
+//             break;
+//         case 9:
+//             printf("hello ..., get a signal: %d\n", signo);
+//             break;
+//         default:
+//             printf("get signo: %d\n", signo);
+//             break;
+//     }
+//     exit(1);
+// }
+
+// // .mytest signo who
+// int main(int argc, char *argv[])
+// {
+//     // 捕获所有信号
+//     int sig = 1;
+//     for (; sig < 32; sig++)
+//     {
+//         signal(sig, handler);
+//     }
+
+//     int res = alarm(30);
+    
+//     while (1)
+//     {
+//         printf("I am a process\n");
+//         sleep(1);
+
+//         int res = alarm(0); // 取消闹钟
+//         printf("res:%d\n", res);
+//     }
+//     return 0;
+// }
 
 // void handler(int signo)
 // {
