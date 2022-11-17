57ae828a2eb1b649ae1322d70d8a334d38228e30
storm-crypto <1742593818@qq.com>
2022-11-17 23:46:40 +0800

    实验：使用sigpending获取pending表，屏蔽2号信号，打印pending表，然后打印的过程中发送2号信号给进程

--- linux/lesson20/mysig.c
+++ linux/lesson20/mysig.c
@@ -2,8 +2,33 @@
 #include <signal.h>
 #include <unistd.h>
 
+// 显示pending表
+void show_pending(sigset_t *set)
+{
+    printf("cur pending:");
+    int i = 1;
+    for (; i <= 31; i++)
+    {
+        if (sigismember(set, i)){
+            printf("1");
+        }
+        else
+        {
+            printf("0");
+        }
+    }
+
+    printf("\n");
+}
+
 int main()
 {
+    // 虽然sigset_t是一个位图结构，但是不同的OS实现是不一样的，不能让用户直接修改该变量
+    // 需要使用特定的函数
+
+    // set是要给变量，保存在用户栈上
+    // sigset_t是不能直接进行|那些操作的
+
     sigset_t iset, oset;
 
     sigemptyset(&iset);
@@ -14,10 +39,16 @@ int main()
     // 1. 设置当前进程的屏蔽字
     // 2. 获取当前进程的屏蔽字
     sigprocmask(SIG_SETMASK, &iset, & oset);
-
+    
+    sigset_t pending;
     while (1)
     {
-        printf("hello bit!\n");
+        sigemptyset(&pending); // 初始化为全0
+
+        sigpending(&pending); // 获取pending表
+
+        show_pending(&pending);
+
         sleep(1);
     }
 
