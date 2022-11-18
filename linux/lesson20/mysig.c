08da659a5442cfa504302b7dac746f4e7240b333
storm-crypto <1742593818@qq.com>
2022-11-19 00:42:27 +0800

    sigacion函数，将2号信号的递达改成自定义的，用到了sigaction结构体的sa_handler参数

--- linux/lesson20/mysig.c
+++ linux/lesson20/mysig.c
@@ -1,72 +1,96 @@
 #include <stdio.h>
 #include <signal.h>
+#include <string.h>
 #include <unistd.h>
-
-// 显示pending表
-void show_pending(sigset_t *set)
-{
-    printf("cur pending:");
-    int i = 1;
-    for (; i <= 31; i++)
-    {
-        if (sigismember(set, i)){
-            printf("1");
-        }
-        else
-        {
-            printf("0");
-        }
-    }
-
-    printf("\n");
-}
 
 void handler(int signo)
 {
-    printf("%d号信号被递达了, 处理已完成\n", signo);
+    printf("get a signo: %d\n", signo);
 }
 
 int main()
 {
-    // 虽然sigset_t是一个位图结构，但是不同的OS实现是不一样的，不能让用户直接修改该变量
-    // 需要使用特定的函数
+    struct sigaction act;
+    memset(&act, 0, sizeof act);
 
-    // set是要给变量，保存在用户栈上
-    // sigset_t是不能直接进行|那些操作的
+    act.sa_handler = handler;
 
-    sigset_t iset, oset;
+    sigaction(2, &act, NULL);
 
-    signal(2, handler);
-
-    sigemptyset(&iset);
-    sigemptyset(&oset);
-
-    sigaddset(&iset, 2);
-
-    // 1. 设置当前进程的屏蔽字
-    // 2. 获取当前进程的屏蔽字
-    sigprocmask(SIG_SETMASK, &iset, & oset);
-    
-    sigset_t pending;
-
-    int count = 0;
     while (1)
     {
-        sigemptyset(&pending); // 初始化为全0
-
-        sigpending(&pending); // 获取pending表
-
-        show_pending(&pending);
-
+        printf("hello bit\n");
         sleep(1);
-        count ++;
-        
-        if (count == 10)
-        {
-            sigprocmask(SIG_SETMASK, &oset, NULL);
-            printf("2号信号恢复, 可以被递达了\n");
-        }
     }
 
     return 0;
 }
+
+// // 显示pending表
+// void show_pending(sigset_t *set)
+// {
+//     printf("cur pending:");
+//     int i = 1;
+//     for (; i <= 31; i++)
+//     {
+//         if (sigismember(set, i)){
+//             printf("1");
+//         }
+//         else
+//         {
+//             printf("0");
+//         }
+//     }
+
+//     printf("\n");
+// }
+
+// void handler(int signo)
+// {
+//     printf("%d号信号被递达了, 处理已完成\n", signo);
+// }
+
+// int main()
+// {
+//     // 虽然sigset_t是一个位图结构，但是不同的OS实现是不一样的，不能让用户直接修改该变量
+//     // 需要使用特定的函数
+
+//     // set是要给变量，保存在用户栈上
+//     // sigset_t是不能直接进行|那些操作的
+
+//     sigset_t iset, oset;
+
+//     signal(2, handler);
+
+//     sigemptyset(&iset);
+//     sigemptyset(&oset);
+
+//     sigaddset(&iset, 2);
+
+//     // 1. 设置当前进程的屏蔽字
+//     // 2. 获取当前进程的屏蔽字
+//     sigprocmask(SIG_SETMASK, &iset, & oset);
+    
+//     sigset_t pending;
+
+//     int count = 0;
+//     while (1)
+//     {
+//         sigemptyset(&pending); // 初始化为全0
+
+//         sigpending(&pending); // 获取pending表
+
+//         show_pending(&pending);
+
+//         sleep(1);
+//         count ++;
+        
+//         if (count == 10)
+//         {
+//             sigprocmask(SIG_SETMASK, &oset, NULL);
+//             printf("2号信号恢复, 可以被递达了\n");
+//         }
+//     }
+
+//     return 0;
+// }
