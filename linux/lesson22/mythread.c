dd68d3cd16cdea5007e518aee6cc90fc03c0449d
storm-crypto <1742593818@qq.com>
2022-11-20 23:08:45 +0800

    进程控制：多线程创建

--- linux/lesson22/mythread.c
+++ linux/lesson22/mythread.c
@@ -6,19 +6,31 @@ void *thread_run(void *args)
 {
     while (1)
     {
-        printf("我是新线程[%s], 我的线程id是%lu\n", (char*)args, pthread_self());
-        sleep(1);
+        // printf("我是新线程[%s], 我的线程id是%lu\n", (char*)args, pthread_self());
+        sleep(3);
     }
 }
 
 int main()
 {
-    pthread_t tid;
-    pthread_create(&tid, NULL, thread_run, (void *)"new thread");
+    pthread_t tid[5];
+    int i;
+    for (i = 0; i < 5; i++)
+    {
+        pthread_create(tid + i, NULL, thread_run, (void *)"new thread");
+    }
+    
 
     while (1)
     {
-        printf("我是主线程, 我创建的线程id是:%lu, 我自己的线程id是: %lu\n", tid, pthread_self());
+        printf("我是主线程, 我自己的thread id是: %lu\n", pthread_self());
+        printf("######################begin###########################\n");
+        for (i = 0; i < 5; i++)
+        {
+            printf("我创建的线程[%d]是: %lu\n", i, tid[i]);
+        }
+        printf("######################end###########################\n");
+        
         sleep(1);
     }
 
