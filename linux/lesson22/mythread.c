9a9dab7b9271214218337dadfe64687e8dd7be4d
storm-crypto <1742593818@qq.com>
2022-11-20 23:36:05 +0800

    进程控制：验证线程的健壮性差，一个线程崩掉，所有的其他线程都会崩掉

--- linux/lesson22/mythread.c
+++ linux/lesson22/mythread.c
@@ -4,10 +4,18 @@
 
 void *thread_run(void *args)
 {
+    int num = *(int*)args;
     while (1)
     {
-        // printf("我是新线程[%s], 我的线程id是%lu\n", (char*)args, pthread_self());
-        sleep(3);
+        printf("我是新线程[%d], 我的线程id是%lu\n", num, pthread_self());
+        sleep(10);
+        // 野指针问题，会出现segmentaion fault
+        if (num == 3) 
+        {
+            printf("thread number : %d quit\n", num);
+            int* p = NULL;
+            *p = 1000;
+        }
     }
 }
 
@@ -17,19 +25,20 @@ int main()
     int i;
     for (i = 0; i < 5; i++)
     {
-        pthread_create(tid + i, NULL, thread_run, (void *)"new thread");
+        pthread_create(tid + i, NULL, thread_run, (void*)&i);
+        sleep(1);
     }
     
 
     while (1)
     {
         printf("我是主线程, 我自己的thread id是: %lu\n", pthread_self());
-        printf("######################begin###########################\n");
+        printf("###################### begin ###########################\n");
         for (i = 0; i < 5; i++)
         {
             printf("我创建的线程[%d]是: %lu\n", i, tid[i]);
         }
-        printf("######################end###########################\n");
+        printf("###################### end ###########################\n");
         
         sleep(1);
     }
