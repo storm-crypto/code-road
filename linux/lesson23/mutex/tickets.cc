680ddebb8b7947455179fb74a354658f9def6bf3
storm-crypto <1742593818@qq.com>
2022-11-23 12:02:31 +0800

    对抢票这个临界区进行加锁

--- linux/lesson23/mutex/tickets.cc
+++ linux/lesson23/mutex/tickets.cc
@@ -5,36 +5,59 @@
 
 // 抢票逻辑，1000张票，5个线程同时抢
 
-int tickets = 1000;
+// 对临界区进行加锁
+class Ticket{
+    private:
+    int tickets;
+    pthread_mutex_t mtx;
 
-void *ThreadRoutine(void* args)
-{
-    int id = *(int*)args;
-    delete (int*)args;
-
-    while (true)
+    public:
+    Ticket()
+        :tickets(1000)
     {
+        pthread_mutex_init(&mtx, nullptr);
+    }
+
+    void GetTicket()
+    {
+        pthread_mutex_lock(&mtx);
         if (tickets > 0){
-            // 抢票
             usleep(1000);
-            std::cout << "我是[" << id << "] 我要抢的票是：" << tickets << std::endl;
+            std::cout << "我是[" << pthread_self() << "] 我要抢的票是：" << tickets << std::endl;
             tickets--;
             printf("");
         }
-        else{
-            // 没有票
-            break;
+        else
+        {
+            printf("票已经被抢空了\n");
         }
+        pthread_mutex_unlock(&mtx);
+    }
 
+    ~Ticket()
+    {
+        pthread_mutex_destroy(&mtx);
+    }
+};
+
+void *ThreadRoutine(void* args)
+{
+    
+    Ticket *t = (Ticket*) args;
+
+    while (true){
+        t->GetTicket();
     }
 }
 
 int main()
 {
+    Ticket *t = new Ticket();
+
     pthread_t tid[5];
     for (int i = 0; i < 5; i++){
         int *id = new int(i);
-        pthread_create(tid + i, nullptr, ThreadRoutine, (void *)id);
+        pthread_create(tid + i, nullptr, ThreadRoutine, (void *)t);
     }
     
     for (int i = 0; i < 5; i++){
