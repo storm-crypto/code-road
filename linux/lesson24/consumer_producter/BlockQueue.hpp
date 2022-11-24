3a03925907782b43830d188e1f6e8e31f4aa9076
storm-crypto <1742593818@qq.com>
2022-11-25 00:00:00 +0800

    生产者消费者模型： 1. 为了避免饥饿问题，添加了条件变量 2. 用_来区分成员函数和成员变量

--- linux/lesson24/consumer_producter/BlockQueue.hpp
+++ linux/lesson24/consumer_producter/BlockQueue.hpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <queue>
+#include <pthread.h>
 
 namespace ns_blockqueue
 {
@@ -9,8 +10,13 @@ namespace ns_blockqueue
     class BlockQueue
     {
     private:
-        std::queue<T> bq; // 阻塞队列
-        int cap;          // 队列的元素上限
+        std::queue<T> _bq;    // 阻塞队列
+        int _cap;             // 队列的元素上限
+        pthread_mutex_t _mtx; // 保护临界资源的锁
+
+        pthread_cond_t _is_full;  // _bq满的，消费者在该条件变量下等待
+        pthread_cond_t _is_empty; //_bq空的，生产者在该条件变量下等待
+
     public:
         BlockQueue()
         {
