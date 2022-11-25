798ff5cb379f0c64ef21e2e8684a59081b9067fa
storm-crypto <1742593818@qq.com>
2022-11-25 23:47:12 +0800

    环形队列实现生产消费模型：定义信号量，初始化信号量，销毁信号量

--- linux/lesson24/ring_buffer/ring_queue.hpp
+++ linux/lesson24/ring_buffer/ring_queue.hpp
@@ -2,18 +2,45 @@
 
 #include <iostream>
 #include <vector>
+#include <semaphore.h>
 
 namespace ns_ring_queue
 {
+    const int g_cap_default = 10;
+
     template <class T>
     class RingQueue
     {
     private:
         std::vector<T> _ring_queue;
         int _cap;
+        // 生产者关心空位置资源
+        sem_t _blank_sem_;
+        // 消费者关心数据资源
+        sem_t _data_sem_;
 
     public:
-        RingQueue() {}
-        ~RingQueue() {}
+        RingQueue(int cap = g_cap_default)
+            : _cap = cap,
+              _ring_queue(cap)
+        {
+            sem_init(&_blank_sem_, 0, cap);
+            sem_init(&_data_sem_, 0, 0); // 数据信号量的初始值为0
+        }
+        ~RingQueue()
+        {
+            sem_destroy(&_blank_sem_);
+            sem_destroy(&_data_sem_);
+        }
+
+    public:
+        void Push(const T &in)
+        {
+            // 生产接口
+        }
+        void Pop(T *out)
+        {
+            // 消费接口
+        }
     };
 }
