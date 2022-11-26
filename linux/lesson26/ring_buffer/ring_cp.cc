70a2e87bd1f9a56738b7fc1004910b81c630de30
storm-crypto <1742593818@qq.com>
2022-11-26 13:08:33 +0800

    环形队列的多生产者多消费者模型：测试代码

--- linux/lesson26/ring_buffer/ring_cp.cc
+++ linux/lesson26/ring_buffer/ring_cp.cc
@@ -12,7 +12,7 @@ void *consumer(void *args)
     {
         int data = 0;
         rq->Pop(&data);
-        std::cout << "消费数据是：" << data << std::endl;
+        std::cout << "消费数据是：" << data << "   我是" << pthread_self() << std::endl;
         // sleep(1);
     }
 }
@@ -23,7 +23,7 @@ void *producter(void *args)
     while (true)
     {
         int data = rand() % 20 + 1;
-        std::cout << "生产数据是：" << data << std::endl;
+        std::cout << "生产数据是：" << data << "  我是：" << pthread_self() << std::endl;
         rq->Push(data);
         sleep(1);
     }
@@ -34,12 +34,24 @@ int main()
     srand((long long)time(nullptr));
     RingQueue<int> *rq = new RingQueue<int>();
 
-    pthread_t c, p;
-    pthread_create(&c, nullptr, consumer, (void *)rq);
-    pthread_create(&p, nullptr, producter, (void *)rq);
+    pthread_t c0, c1, c2, c3, p0, p1, p2;
+    pthread_create(&c0, nullptr, consumer, (void *)rq);
+    pthread_create(&c1, nullptr, consumer, (void *)rq);
+    pthread_create(&c2, nullptr, consumer, (void *)rq);
+    pthread_create(&c3, nullptr, consumer, (void *)rq);
 
-    pthread_join(c, nullptr);
-    pthread_join(p, nullptr);
+    pthread_create(&p0, nullptr, producter, (void *)rq);
+    pthread_create(&p1, nullptr, producter, (void *)rq);
+    pthread_create(&p2, nullptr, producter, (void *)rq);
+
+    pthread_join(c0, nullptr);
+    pthread_join(c1, nullptr);
+    pthread_join(c2, nullptr);
+    pthread_join(c3, nullptr);
+
+    pthread_join(p0, nullptr);
+    pthread_join(p1, nullptr);
+    pthread_join(p2, nullptr);
 
     return 0;
 }
