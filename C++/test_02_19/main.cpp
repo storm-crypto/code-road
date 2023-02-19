f516bb76aea8fef42bbbd2146f4b0aee1a26ef1c
storm-crypto <1742593818@qq.com>
2023-02-19 23:19:56 +0800

    demo：两个线程同时对一个变量进行++操作(原子操作的++)

--- C++/test_02_19/main.cpp
+++ C++/test_02_19/main.cpp
@@ -2,23 +2,49 @@
 #include <functional>
 #include <thread>
 #include <mutex>
+#include <atomic>
 
 using namespace std;
 
-// 两个线程对同一个变量++
-int x = 0;
-mutex mtx;
+//// 两个线程对同一个变量++
+//int x = 0;
+//mutex mtx;
+//
+//void Func(int n)
+//{
+//	// 加锁，加在循环里面还是循环外面？为什么？
+//	mtx.lock();
+//	for (int i = 0; i < n; i++)
+//	{
+//		cout << this_thread::get_id() << "->" << x << endl; // 看一下是哪个进程在+x
+//		++x;
+//	}
+//	mtx.unlock();
+//
+//}
+//
+//int main()
+//{
+//	thread t1(Func, 10);
+//	thread t2(Func, 20);
+//
+//	t1.join();
+//	t2.join();
+//
+//	cout << x << endl;
+//
+//	return 0;
+//}
+
+// 使用原子操作的++
+atomic<int> x = 0;
 
 void Func(int n)
 {
-	// 加锁，加在循环里面还是循环外面？为什么？
-	mtx.lock();
 	for (int i = 0; i < n; i++)
 	{
-		cout << this_thread::get_id() << "->" << x << endl; // 看一下是哪个进程在+x
 		++x;
 	}
-	mtx.unlock();
 
 }
 
