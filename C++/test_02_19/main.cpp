741a260debd4817bae92702e7274641b05cd15e3
storm-crypto <1742593818@qq.com>
2023-02-19 23:28:48 +0800

    demo：两个线程同时对一个变量进行++操作(使用lambda表达式 + 锁(串行的那种) + 统计耗费的时间)

--- C++/test_02_19/main.cpp
+++ C++/test_02_19/main.cpp
@@ -37,26 +37,55 @@ using namespace std;
 //}
 
 // 使用原子操作的++
-atomic<int> x = 0;
+//atomic<int> x = 0;
+//
+//void Func(int n)
+//{
+//	for (int i = 0; i < n; i++)
+//	{
+//		++x;
+//	}
+//
+//}
 
-void Func(int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		++x;
-	}
-
-}
-
+// 使用lambda表达式 + 锁的形式
 int main()
 {
-	thread t1(Func, 10);
-	thread t2(Func, 20);
+	int x = 0;
+	mutex mtx;
+	int N = 10000000;
+	int costTime1 = 0;
+
+	thread t1([&]{
+		int begin1 = clock();
+		mtx.lock();
+		for (int i = 0; i < N; i++)
+		{
+			++x;
+		}
+		mtx.unlock();
+		int end1 = clock();
+
+		costTime1 += (end1 - begin1);
+	});
+
+	thread t2([&]{
+	  int begin2 = clock();
+	  mtx.lock();
+	  for (int i = 0; i < N; i++)
+	  {
+		  ++x;
+	  }
+	  mtx.unlock();
+	  int end2 = clock();
+	  costTime1 += (end2 - begin2);
+
+	});
 
 	t1.join();
 	t2.join();
 
-	cout << x << endl;
+	cout << x  << ":" << costTime1 << endl;
 
 	return 0;
 }
